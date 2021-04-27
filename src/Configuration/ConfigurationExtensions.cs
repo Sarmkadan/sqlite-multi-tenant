dc905980dce23b2d6a5f99492949b1c4151896d7
Vladyslav Zaiets <rutova2@gmail.com>
2021-04-27 15:42:20 +0300

    cleanup in BackupController

---
+++ src/Configuration/ConfigurationExtensions.cs
@@ -0,0 +1,226 @@
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using Microsoft.Extensions.Configuration;
+
+namespace SqliteMultiTenant.Configuration;
+
+/// <summary>
+/// Extension methods for application configuration management.
+/// Simplifies reading and validating configuration values with safe defaults.
+/// </summary>
+public static class ConfigurationExtensions
+{
+    /// <summary>
+    /// Reads configuration value with type safety and fallback.
+    /// Returns default value if key doesn't exist or can't be parsed.
+    /// </summary>
+    public static T GetValueSafe<T>(this IConfiguration config, string key, T defaultValue = default)
+    {
+        try
+        {
+            var value = config[key];
+
+            if (string.IsNullOrEmpty(value))
+                return defaultValue;
+
+            if (typeof(T) == typeof(string))
+                return (T)(object)value;
+
+            if (typeof(T) == typeof(int))
+                return int.TryParse(value, out var intResult) ? (T)(object)intResult : defaultValue;
+
+            if (typeof(T) == typeof(bool))
+                return bool.TryParse(value, out var boolResult) ? (T)(object)boolResult : defaultValue;
+
+            if (typeof(T) == typeof(long))
+                return long.TryParse(value, out var longResult) ? (T)(object)longResult : defaultValue;
+
+            if (typeof(T) == typeof(TimeSpan))
+            {
+                if (TimeSpan.TryParse(value, out var timeSpan))
+                    return (T)(object)timeSpan;
+                return defaultValue;
+            }
+
+            return defaultValue;
+        }
+        catch
+        {
+            return defaultValue;
+        }
+    }
+
+    /// <summary>
+    /// Validates required configuration exists and is not empty.
+    /// Throws InvalidOperationException if missing.
+    /// </summary>
+    public static string GetRequiredValue(this IConfiguration config, string key)
+    {
+        var value = config[key];
+
+        if (string.IsNullOrWhiteSpace(value))
+            throw new InvalidOperationException($"Required configuration '{key}' is missing or empty");
+
+        return value;
+    }
+
+    /// <summary>
+    /// Binds configuration section to strongly-typed object.
+    /// </summary>
+    public static T BindSection<T>(this IConfiguration config, string sectionKey) where T : new()
+    {
+        var section = config.GetSection(sectionKey);
+        var result = new T();
+
+        section.Bind(result);
+
+        return result;
+    }
+
+    /// <summary>
+    /// Gets connection string with fallback and validation.
+    /// </summary>
+    public static string GetConnectionStringSafe(this IConfiguration config, string name, string defaultValue = null)
+    {
+        var connectionString = config.GetConnectionString(name);
+
+        if (string.IsNullOrEmpty(connectionString))
+            return defaultValue ?? throw new InvalidOperationException($"Connection string '{name}' not found");
+
+        return connectionString;
+    }
+
+    /// <summary>
+    /// Checks if configuration key exists and has a value.
+    /// </summary>
+    public static bool HasValue(this IConfiguration config, string key)
+    {
+        return !string.IsNullOrEmpty(config[key]);
+    }
+
+    /// <summary>
+    /// Gets all configuration values for a section as dictionary.
+    /// Useful for exporting configuration or debugging.
+    /// </summary>
+    public static Dictionary<string, string> GetSectionAsDictionary(this IConfiguration config, string sectionKey)
+    {
+        var section = config.GetSection(sectionKey);
+        var result = new Dictionary<string, string>();
+
+        foreach (var child in section.GetChildren())
+        {
+            result[child.Key] = child.Value;
+        }
+
+        return result;
+    }
+
+    /// <summary>
+    /// Validates configuration follows expected schema.
+    /// Checks for required keys and expected value ranges.
+    /// </summary>
+    public static IEnumerable<string> ValidateConfiguration(this IConfiguration config, params string[] requiredKeys)
+    {
+        var errors = new List<string>();
+
+        foreach (var key in requiredKeys)
+        {
+            if (!config.HasValue(key))
+                errors.Add($"Required configuration key '{key}' is missing");
+        }
+
+        return errors;
+    }
+
+    /// <summary>
+    /// Reloads configuration from sources (if supported).
+    /// Useful for hot-reload scenarios.
+    /// </summary>
+    public static void Reload(this IConfigurationRoot config)
+    {
+        config.Reload();
+    }
+
+    /// <summary>
+    /// Gets configuration value with environment variable override.
+    /// Allows sensitive values to be set via environment variables.
+    /// </summary>
+    public static string GetValueWithEnvironmentOverride(this IConfiguration config, string key, string envVar = null)
+    {
+        var envVarName = envVar ?? key.ToUpper().Replace(":", "_");
+        var envValue = Environment.GetEnvironmentVariable(envVarName);
+
+        if (!string.IsNullOrEmpty(envValue))
+            return envValue;
+
+        return config[key];
+    }
+}
+
+/// <summary>
+/// Helper for building configuration from multiple sources.
+/// Centralizes configuration setup logic.
+/// </summary>
+public class ConfigurationBuilder
+{
+    private readonly Microsoft.Extensions.Configuration.ConfigurationBuilder _builder;
+
+    public ConfigurationBuilder()
+    {
+        _builder = new Microsoft.Extensions.Configuration.ConfigurationBuilder();
+    }
+
+    /// <summary>
+    /// Adds JSON configuration file.
+    /// </summary>
+    public ConfigurationBuilder AddJsonFile(string path, bool optional = false, bool reloadOnChange = true)
+    {
+        _builder.AddJsonFile(path, optional, reloadOnChange);
+        return this;
+    }
+
+    /// <summary>
+    /// Adds environment variables with prefix.
+    /// </summary>
+    public ConfigurationBuilder AddEnvironmentVariables(string prefix = null)
+    {
+        _builder.AddEnvironmentVariables(prefix);
+        return this;
+    }
+
+    /// <summary>
+    /// Adds in-memory configuration from dictionary.
+    /// </summary>
+    public ConfigurationBuilder AddInMemory(Dictionary<string, string> settings)
+    {
+        _builder.AddInMemoryCollection(settings);
+        return this;
+    }
+
+    /// <summary>
+    /// Builds final configuration.
+    /// </summary>
+    public IConfigurationRoot Build()
+    {
+        return _builder.Build();
+    }
+
+    /// <summary>
+    /// Creates builder with standard configuration for application.
+    /// Loads: appsettings.json -> appsettings.{env}.json -> environment variables
+    /// </summary>
+    public static IConfigurationRoot BuildStandardConfiguration(string environment = null)
+    {
+        var env = environment ?? Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
+
+        return new Microsoft.Extensions.Configuration.ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{env}.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables()
+            .Build();
+    }
+}
---
+++ src/Utilities/ValidationExtensions.cs
@@ -0,0 +1,172 @@
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using System.Text.RegularExpressions;
+
+namespace SqliteMultiTenant.Utilities;
+
+/// <summary>
+/// Extension methods for common validation patterns in the application.
+/// Used at system boundaries (controllers, services) to validate user input.
+/// Returns bool to enable fluent validation chains.
+/// </summary>
+public static class ValidationExtensions
+{
+    /// <summary>
+    /// Validates email address format using RFC 5322 simplified pattern.
+    /// More permissive than strict RFC but catches obvious errors.
+    /// </summary>
+    public static bool IsValidEmail(this string email)
+    {
+        if (string.IsNullOrWhiteSpace(email))
+            return false;
+
+        try
+        {
+            var pattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+            return Regex.IsMatch(email, pattern, RegexOptions.IgnoreCase);
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// Validates UUID format (v4 preferred).
+    /// Accepts both with and without hyphens (e.g., both formats valid).
+    /// </summary>
+    public static bool IsValidUuid(this string uuid)
+    {
+        return Guid.TryParse(uuid, out _);
+    }
+
+    /// <summary>
+    /// Validates a semantic version string (e.g., "1.0.0", "2.3.4-beta").
+    /// Allows prerelease and build metadata per SemVer 2.0.0 spec.
+    /// </summary>
+    public static bool IsValidSemanticVersion(this string version)
+    {
+        if (string.IsNullOrWhiteSpace(version))
+            return false;
+
+        var pattern = @"^\d+\.\d+\.\d+(-[a-zA-Z0-9.]+)?(\+[a-zA-Z0-9.]+)?$";
+        return Regex.IsMatch(version, pattern);
+    }
+
+    /// <summary>
+    /// Validates database name contains only safe characters.
+    /// Prevents SQL injection by restricting to alphanumeric + underscore.
+    /// </summary>
+    public static bool IsValidDatabaseName(this string name)
+    {
+        if (string.IsNullOrWhiteSpace(name) || name.Length > 255)
+            return false;
+
+        return Regex.IsMatch(name, @"^[a-zA-Z_][a-zA-Z0-9_]*$");
+    }
+
+    /// <summary>
+    /// Validates tenant name is non-empty and reasonable length.
+    /// Used to prevent storage bloat from extremely long names.
+    /// </summary>
+    public static bool IsValidTenantName(this string name)
+    {
+        return !string.IsNullOrWhiteSpace(name) && name.Length >= 3 && name.Length <= 255;
+    }
+
+    /// <summary>
+    /// Validates a file path is within allowed directory and doesn't traverse up.
+    /// Prevents "../../" attack vectors in user-provided paths.
+    /// </summary>
+    public static bool IsValidRelativePath(this string path)
+    {
+        if (string.IsNullOrWhiteSpace(path) || path.Contains(".."))
+            return false;
+
+        // Only allow forward/backward slashes, alphanumeric, dots, hyphens
+        return Regex.IsMatch(path, @"^[a-zA-Z0-9._\-/\\]+$");
+    }
+
+    /// <summary>
+    /// Validates SQL script is not empty and doesn't contain dangerous patterns.
+    /// Catches obvious SQL injection attempts without full parsing.
+    /// </summary>
+    public static bool IsValidSqlScript(this string script)
+    {
+        if (string.IsNullOrWhiteSpace(script))
+            return false;
+
+        // Reject scripts with dangerous patterns (basic check)
+        var dangerousPatterns = new[] { "DROP DATABASE", "DELETE FROM", "TRUNCATE" };
+        var upperScript = script.ToUpper();
+
+        return !dangerousPatterns.Any(pattern => upperScript.Contains(pattern));
+    }
+
+    /// <summary>
+    /// Validates a port number is in valid range (1-65535).
+    /// Used for connection string validation.
+    /// </summary>
+    public static bool IsValidPort(this int port)
+    {
+        return port >= 1 && port <= 65535;
+    }
+
+    /// <summary>
+    /// Validates connection string contains required components.
+    /// Checks for Data Source or Filename (SQLite) and basic format.
+    /// </summary>
+    public static bool IsValidConnectionString(this string connectionString)
+    {
+        if (string.IsNullOrWhiteSpace(connectionString))
+            return false;
+
+        // For SQLite: check for Data Source or Filename
+        var hasDataSource = connectionString.Contains("Data Source=", StringComparison.OrdinalIgnoreCase) ||
+                           connectionString.Contains("Filename=", StringComparison.OrdinalIgnoreCase);
+
+        return hasDataSource;
+    }
+
+    /// <summary>
+    /// Validates backup tag is within acceptable length and characters.
+    /// Tags are used for categorization and searching.
+    /// </summary>
+    public static bool IsValidBackupTag(this string tag)
+    {
+        if (string.IsNullOrWhiteSpace(tag) || tag.Length > 100)
+            return false;
+
+        return Regex.IsMatch(tag, @"^[a-zA-Z0-9\-_]+$");
+    }
+
+    /// <summary>
+    /// Checks if a collection is null or empty.
+    /// Enables fluent validation for lists and arrays.
+    /// </summary>
+    public static bool IsNullOrEmpty<T>(this IEnumerable<T> collection)
+    {
+        return collection == null || !collection.Any();
+    }
+
+    /// <summary>
+    /// Validates retention days is reasonable (between 1 and 3650 days ~10 years).
+    /// Prevents configuration errors from setting unrealistic retention.
+    /// </summary>
+    public static bool IsValidRetentionDays(this int days)
+    {
+        return days >= 1 && days <= 3650;
+    }
+
+    /// <summary>
+    /// Validates connection timeout is within reasonable bounds.
+    /// Prevents extremely long timeouts that could cause application hangs.
+    /// </summary>
+    public static bool IsValidConnectionTimeout(this int timeoutSeconds)
+    {
+        return timeoutSeconds >= 1 && timeoutSeconds <= 300;
+    }
+}
