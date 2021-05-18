bf5fcaaeae0be977bd8e5929232ef1efd267842d
Vladyslav Zaiets <rutova2@gmail.com>
2021-05-18 10:40:43 +0300

    Fix

--- src/Services/IBackupService.cs
+++ src/Services/IBackupService.cs
@@ -10,22 +10,114 @@ using SqliteMultiTenant.Models;
 namespace SqliteMultiTenant.Services;
 
 /// <summary>
-/// Service interface for database backup management
+/// Service interface for database backup management.
 /// </summary>
 public interface IBackupService
 {
+    /// <summary>
+    /// Retrieves a backup by its ID.
+    /// </summary>
+    /// <param name="backupId">The unique ID of the backup.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>The backup if found; otherwise, null.</returns>
     Task<Backup?> GetBackupAsync(string backupId, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Retrieves all backups for a specific database.
+    /// </summary>
+    /// <param name="databaseId">The database ID.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>A list of backups.</returns>
     Task<List<Backup>> GetDatabaseBackupsAsync(string databaseId, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Retrieves completed backups for a specific database.
+    /// </summary>
+    /// <param name="databaseId">The database ID.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>A list of completed backups.</returns>
     Task<List<Backup>> GetCompletedBackupsAsync(string databaseId, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Retrieves the latest backup for a specific database.
+    /// </summary>
+    /// <param name="databaseId">The database ID.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>The latest backup if found; otherwise, null.</returns>
     Task<Backup?> GetLatestBackupAsync(string databaseId, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Creates a new backup record.
+    /// </summary>
+    /// <param name="databaseId">The database ID.</param>
+    /// <param name="backupType">The type of backup.</param>
+    /// <param name="createdBy">The user or process creating the backup.</param>
+    /// <param name="backupPath">Optional path for the backup file.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>The created backup record.</returns>
     Task<Backup> CreateBackupAsync(string databaseId, BackupType backupType, string createdBy, string? backupPath = null, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Marks a backup as completed.
+    /// </summary>
+    /// <param name="backupId">The backup ID.</param>
+    /// <param name="sizeBytes">The size of the backup in bytes.</param>
+    /// <param name="durationMs">The duration of the backup in milliseconds.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
     Task MarkBackupAsCompletedAsync(string backupId, long sizeBytes, long durationMs, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Marks a backup as failed.
+    /// </summary>
+    /// <param name="backupId">The backup ID.</param>
+    /// <param name="errorMessage">The error message.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
     Task MarkBackupAsFailedAsync(string backupId, string errorMessage, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Verifies the integrity of a backup.
+    /// </summary>
+    /// <param name="backupId">The backup ID.</param>
+    /// <param name="verifiedBy">The user or process verifying the backup.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
     Task VerifyBackupAsync(string backupId, string verifiedBy, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Sets an expiration date for a backup.
+    /// </summary>
+    /// <param name="backupId">The backup ID.</param>
+    /// <param name="expirationDate">The expiration date.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
     Task SetBackupExpirationAsync(string backupId, DateTime expirationDate, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Retrieves all expired backups.
+    /// </summary>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>A list of expired backups.</returns>
     Task<List<Backup>> GetExpiredBackupsAsync(CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Gets the count of backups for a database.
+    /// </summary>
+    /// <param name="databaseId">The database ID.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>The backup count.</returns>
     Task<int> GetBackupCountAsync(string databaseId, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Deletes a backup.
+    /// </summary>
+    /// <param name="backupId">The backup ID.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
     Task DeleteBackupAsync(string backupId, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Adds a tag to a backup.
+    /// </summary>
+    /// <param name="backupId">The backup ID.</param>
+    /// <param name="tag">The tag to add.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
     Task AddBackupTagAsync(string backupId, string tag, CancellationToken cancellationToken = default);
 
     /// <summary>
--- src/Services/IMigrationService.cs
+++ src/Services/IMigrationService.cs
@@ -9,20 +9,108 @@ using SqliteMultiTenant.Models;
 namespace SqliteMultiTenant.Services;
 
 /// <summary>
-/// Service interface for database migration management
+/// Service interface for database migration management.
 /// </summary>
 public interface IMigrationService
 {
+    /// <summary>
+    /// Retrieves a migration by its ID.
+    /// </summary>
+    /// <param name="migrationId">The unique ID of the migration.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>The migration if found; otherwise, null.</returns>
     Task<Migration?> GetMigrationAsync(string migrationId, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Retrieves all migrations for a specific database.
+    /// </summary>
+    /// <param name="databaseId">The database ID.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>A list of migrations.</returns>
     Task<List<Migration>> GetDatabaseMigrationsAsync(string databaseId, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Retrieves pending migrations for a database.
+    /// </summary>
+    /// <param name="databaseId">The database ID.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>A list of pending migrations.</returns>
     Task<List<Migration>> GetPendingMigrationsAsync(string databaseId, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Retrieves applied migrations for a database.
+    /// </summary>
+    /// <param name="databaseId">The database ID.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>A list of applied migrations.</returns>
     Task<List<Migration>> GetAppliedMigrationsAsync(string databaseId, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Creates a new migration definition.
+    /// </summary>
+    /// <param name="databaseId">The database ID.</param>
+    /// <param name="version">The migration version.</param>
+    /// <param name="name">The migration name.</param>
+    /// <param name="upScript">The SQL script to apply the migration.</param>
+    /// <param name="downScript">Optional SQL script to rollback the migration.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>The created migration.</returns>
     Task<Migration> CreateMigrationAsync(string databaseId, string version, string name, string upScript, string? downScript = null, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Executes a migration.
+    /// </summary>
+    /// <param name="migrationId">The migration ID.</param>
+    /// <param name="executedBy">The user or process executing the migration.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
     Task ExecuteMigrationAsync(string migrationId, string executedBy, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Rolls back a migration.
+    /// </summary>
+    /// <param name="migrationId">The migration ID.</param>
+    /// <param name="executedBy">The user or process rolling back the migration.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
     Task RollbackMigrationAsync(string migrationId, string executedBy, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Marks a migration as completed.
+    /// </summary>
+    /// <param name="migrationId">The migration ID.</param>
+    /// <param name="executionTimeMs">The time taken to execute in milliseconds.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
     Task MarkMigrationAsCompletedAsync(string migrationId, long executionTimeMs, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Marks a migration as failed.
+    /// </summary>
+    /// <param name="migrationId">The migration ID.</param>
+    /// <param name="errorMessage">The error message.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
     Task MarkMigrationAsFailedAsync(string migrationId, string errorMessage, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Gets the count of migrations for a database.
+    /// </summary>
+    /// <param name="databaseId">The database ID.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>The migration count.</returns>
     Task<int> GetMigrationCountAsync(string databaseId, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Checks if a migration version is applied for a database.
+    /// </summary>
+    /// <param name="databaseId">The database ID.</param>
+    /// <param name="version">The migration version.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>True if applied; otherwise, false.</returns>
     Task<bool> IsMigrationAppliedAsync(string databaseId, string version, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Retrieves failed migrations for a database.
+    /// </summary>
+    /// <param name="databaseId">The database ID.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>A list of failed migrations.</returns>
     Task<List<Migration>> GetFailedMigrationsAsync(string databaseId, CancellationToken cancellationToken = default);
 }
