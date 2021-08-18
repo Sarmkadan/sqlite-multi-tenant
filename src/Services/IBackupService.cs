7d7c4bd523b17d9e4689c34fe3ac3f47fa50801c
Vladyslav Zaiets <rutova2@gmail.com>
2021-08-18 15:36:46 +0300

    Tidy up BackupPorgress

--- src/Services/IBackupService.cs
+++ src/Services/IBackupService.cs
@@ -27,4 +27,28 @@ public interface IBackupService
     Task<int> GetBackupCountAsync(string databaseId, CancellationToken cancellationToken = default);
     Task DeleteBackupAsync(string backupId, CancellationToken cancellationToken = default);
     Task AddBackupTagAsync(string backupId, string tag, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Performs a physical SQLite online backup of <paramref name="sourceDatabasePath"/> to
+    /// <paramref name="destinationPath"/>, reporting page-level progress via
+    /// <paramref name="progress"/> as the copy proceeds.
+    /// </summary>
+    /// <param name="sourceDatabasePath">Absolute path to the source SQLite database file.</param>
+    /// <param name="destinationPath">Absolute path where the backup file should be written.</param>
+    /// <param name="progress">
+    /// Optional receiver for <see cref="BackupProgress"/> updates. Each callback fires
+    /// after a batch of pages has been copied. Pass <c>null</c> to skip reporting.
+    /// </param>
+    /// <param name="pagesPerStep">
+    /// Number of pages to copy per step. Smaller values yield more granular progress
+    /// updates; larger values reduce callback overhead. Default: <c>-1</c> (copy all
+    /// pages in one step, equivalent to a single-shot backup).
+    /// </param>
+    /// <param name="cancellationToken">Token used to abort the backup mid-flight.</param>
+    Task BackupWithProgressAsync(
+        string sourceDatabasePath,
+        string destinationPath,
+        IProgress<BackupProgress>? progress = null,
+        int pagesPerStep = -1,
+        CancellationToken cancellationToken = default);
 }
