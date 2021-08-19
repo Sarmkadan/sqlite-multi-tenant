83f11d2cb5f744549be74ed161a832fa74c0a2eb
Vladyslav Zaiets <rutova2@gmail.com>
2021-08-19 18:13:55 +0300

    refactor: dedupe some logic in IBackupService

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -1,109 +1,24 @@
 # Contributing to sqlite-multi-tenant
 
-Thank you for considering contributing to sqlite-multi-tenant! This guide will help you get started.
+Contributions are welcome! Please follow these guidelines:
 
-## How to Contribute
+1. **Fork** the repository and **clone** your fork.
+2. **Create a branch** for your feature or bug fix (`git checkout -b feature/name`).
+3. **Make your changes** and commit with descriptive messages.
+4. **Run tests** (`dotnet test`) to ensure everything works.
+5. **Push** to your fork and **submit a Pull Request**.
 
-1. **Fork the repository** on GitHub.
-2. **Clone your fork** locally:
-   ```bash
-   git clone https://github.com/YOUR_USERNAME/sqlite-multi-tenant.git
-   cd sqlite-multi-tenant
-   ```
-3. **Create a new branch** for your feature or bug fix:
-   ```bash
-   git checkout -b feature/your-feature-name
-   ```
-4. **Make your changes** and commit them with descriptive messages.
-5. **Run the tests** locally to ensure everything works as expected:
-   ```bash
-   dotnet test
-   ```
-6. **Push to your fork** on GitHub.
-7. **Submit a Pull Request** to the main repository.
+## Requirements
 
-## Development Requirements
+- .NET 10.0 SDK
+- SQLite (via NuGet)
 
-- **.NET 10.0 SDK** or later
-- SQLite (bundled via NuGet, no separate install needed)
+## Guidelines
 
-## Building the Project
-
-```bash
-# Restore dependencies
-dotnet restore
-
-# Build the solution (Release configuration)
-dotnet build --configuration Release
-
-# Run all tests
-dotnet test --configuration Release --verbosity normal --logger "trx;LogFileName=test-results.trx"
-
-# Run benchmarks
-dotnet run --project benchmarks/sqlite-multi-tenant.Benchmarks -c Release
-
-# Run with Docker
-docker-compose up --build
-```
-
-## CI / CD
-
-All pull requests run the CI workflow automatically (`ci.yml`). Ensure your branch passes before requesting review:
-
-- `ci.yml` — build and test on every push/PR to `main`
-- `release.yml` — triggered on `v*` tags; packs and publishes the NuGet package and creates a GitHub release
-- `docker.yml` — builds and pushes the container image to `ghcr.io/sarmkadan/sqlite-multi-tenant`
-
-## Project Structure
-
-```
-src/
-  Api/              - REST controllers and request/response models
-  BackgroundWorkers/ - Scheduled tasks (backup rotation, maintenance)
-  Caching/          - In-memory and distributed cache services
-  Cli/              - Command-line interface
-  Configuration/    - DI setup and options
-  Database/         - Connection pooling and schema management
-  DataOperations/   - Query builders and data import/export
-  Events/           - Domain event bus and handlers
-  Middleware/       - ASP.NET middleware (rate limiting, logging, etc.)
-  Models/           - Domain entities (Tenant, Backup, Migration)
-  Monitoring/       - Metrics, audit logging, performance monitoring
-  Repositories/     - Data access layer
-  Security/         - Encryption key management, rate limiting
-  Services/         - Business logic layer
-  Tenants/          - Tenant provisioning, isolation, recovery
-  Utilities/        - Extension methods and helpers
-  Validation/       - Fluent validation rules
-tests/              - Unit and integration tests
-```
-
-## Code Style
-
-- Follow the existing coding conventions found in the repository.
-- Use XML documentation (`/// <summary>`) for all public classes, methods, and properties.
-- **Keep all author headers intact.** Do not remove existing author headers from source files.
-- Use `sealed` on classes that are not designed for inheritance.
-- Prefer `async/await` with `CancellationToken` support for I/O operations.
-- Use structured logging with `ILogger` and named placeholders (not string interpolation).
-
-## Pull Request Guidelines
-
+- Follow existing code style.
+- Add XML documentation for public APIs.
+- Include unit tests for new features.
 - Keep PRs focused on a single concern.
-- Include tests for new functionality.
-- Update XML documentation for any changed public APIs.
-- Ensure `dotnet build` completes without warnings.
+- Ensure all tests pass before submitting.
 
-## Reporting Issues
-
-If you find a bug or have a feature request, please use GitHub Issues.
-When reporting an issue, please include:
-- A clear and descriptive title.
-- Steps to reproduce the problem.
-- Expected behavior vs. actual behavior.
-- .NET version and operating system.
-- Any relevant logs or error messages.
-
-## License
-
-By contributing, you agree that your contributions will be licensed under the MIT License.
+Issues and feature requests can be reported via GitHub Issues.
--- src/Services/ITenantService.cs
+++ src/Services/ITenantService.cs
@@ -9,22 +9,114 @@ using SqliteMultiTenant.Models;
 namespace SqliteMultiTenant.Services;
 
 /// <summary>
-/// Service interface for tenant management operations
+/// Service interface for tenant management operations.
 /// </summary>
 public interface ITenantService
 {
+    /// <summary>
+    /// Retrieves a tenant by its unique identifier.
+    /// </summary>
+    /// <param name="tenantId">The unique identifier of the tenant.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>The tenant if found; otherwise, null.</returns>
     Task<Tenant?> GetTenantAsync(string tenantId, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Creates a new tenant.
+    /// </summary>
+    /// <param name="name">The name of the tenant.</param>
+    /// <param name="description">Optional description of the tenant.</param>
+    /// <param name="contactEmail">Optional contact email for the tenant.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>The created tenant.</returns>
     Task<Tenant> CreateTenantAsync(string name, string? description = null, string? contactEmail = null, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Updates the details of an existing tenant.
+    /// </summary>
+    /// <param name="tenant">The tenant object with updated details.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
     Task UpdateTenantAsync(Tenant tenant, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Deletes a tenant.
+    /// </summary>
+    /// <param name="tenantId">The unique identifier of the tenant.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
     Task DeleteTenantAsync(string tenantId, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Retrieves all tenants.
+    /// </summary>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>A list of all tenants.</returns>
     Task<List<Tenant>> GetAllTenantsAsync(CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Retrieves all active tenants.
+    /// </summary>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>A list of active tenants.</returns>
     Task<List<Tenant>> GetActiveTenantsAsync(CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Activates a tenant.
+    /// </summary>
+    /// <param name="tenantId">The unique identifier of the tenant.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
     Task ActivateTenantAsync(string tenantId, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Deactivates a tenant.
+    /// </summary>
+    /// <param name="tenantId">The unique identifier of the tenant.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
     Task DeactivateTenantAsync(string tenantId, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Suspends a tenant.
+    /// </summary>
+    /// <param name="tenantId">The unique identifier of the tenant.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
     Task SuspendTenantAsync(string tenantId, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Checks if a tenant exists.
+    /// </summary>
+    /// <param name="tenantId">The unique identifier of the tenant.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>True if the tenant exists; otherwise, false.</returns>
     Task<bool> TenantExistsAsync(string tenantId, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Gets the count of all tenants.
+    /// </summary>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>The total number of tenants.</returns>
     Task<int> GetTenantCountAsync(CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Searches for tenants based on a search term.
+    /// </summary>
+    /// <param name="searchTerm">The term to search for.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>A list of tenants matching the search term.</returns>
     Task<List<Tenant>> SearchTenantsAsync(string searchTerm, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Sets metadata for a tenant.
+    /// </summary>
+    /// <param name="tenantId">The unique identifier of the tenant.</param>
+    /// <param name="key">The metadata key.</param>
+    /// <param name="value">The metadata value.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
     Task SetTenantMetadataAsync(string tenantId, string key, string value, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Retrieves the database size info for a tenant.
+    /// </summary>
+    /// <param name="tenantId">The unique identifier of the tenant.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>The tenant storage info.</returns>
     Task<TenantStorageInfo> GetTenantDatabaseSizeAsync(string tenantId, CancellationToken cancellationToken = default);
 }
