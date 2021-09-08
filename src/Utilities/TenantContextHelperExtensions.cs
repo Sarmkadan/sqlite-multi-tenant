35a15e101b6ea81c4bd2488435854dd038f67c8f
Vladyslav Zaiets <rutova2@gmail.com>
2021-09-08 06:14:56 +0300

    more TenantContextHelper extension methods
    mostly TenantContextHelperExtensions.cs, nothing exciting.

---
+++ src/Utilities/TenantContextHelperExtensions.cs
@@ -0,0 +1,136 @@
+#nullable enable
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using SqliteMultiTenant.Models;
+
+namespace SqliteMultiTenant.Utilities
+{
+    /// <summary>
+    /// Extension methods for <see cref="TenantContextHelper"/> that provide common tenant context operations.
+    /// </summary>
+    public static class TenantContextHelperExtensions
+    {
+        /// <summary>
+        /// Creates a tenant context scope with automatic tenant ID validation.
+        /// </summary>
+        /// <param name="helper">The tenant context helper instance.</param>
+        /// <param name="tenantId">The tenant ID to set in the context.</param>
+        /// <param name="userId">Optional user ID to associate with the context.</param>
+        /// <returns>An <see cref="IDisposable"/> scope that restores the previous context when disposed.</returns>
+        /// <exception cref="ArgumentException">Thrown when tenantId is null or whitespace.</exception>
+        public static IDisposable CreateValidatedScope(this TenantContextHelper helper, string tenantId, string userId = null)
+        {
+            ArgumentNullException.ThrowIfNull(helper);
+            ArgumentException.ThrowIfNullOrWhiteSpace(tenantId, nameof(tenantId));
+
+            if (!helper.ValidateTenantContext(tenantId))
+            {
+                helper.SetTenantContext(new TenantContext { TenantId = tenantId, UserId = userId, CreatedAt = DateTime.UtcNow });
+            }
+            else
+            {
+                var existingContext = helper.GetTenantContext();
+                helper.SetTenantContext(new TenantContext
+                {
+                    TenantId = tenantId,
+                    UserId = userId ?? existingContext?.UserId,
+                    CreatedAt = DateTime.UtcNow
+                });
+            }
+
+            return helper.CreateScope(tenantId, userId);
+        }
+
+        /// <summary>
+        /// Gets the current tenant ID or throws if not available.
+        /// </summary>
+        /// <param name="helper">The tenant context helper instance.</param>
+        /// <returns>The current tenant ID.</returns>
+        /// <exception cref="InvalidOperationException">Thrown when no tenant context is set.</exception>
+        public static string GetRequiredTenantId(this TenantContextHelper helper)
+        {
+            ArgumentNullException.ThrowIfNull(helper);
+
+            var tenantId = helper.GetCurrentTenantId();
+            if (string.IsNullOrEmpty(tenantId))
+            {
+                throw new InvalidOperationException("Tenant context is not set. Call SetTenantContext first.");
+            }
+
+            return tenantId;
+        }
+
+        /// <summary>
+        /// Executes an action within a tenant context scope, automatically restoring the previous context.
+        /// </summary>
+        /// <param name="helper">The tenant context helper instance.</param>
+        /// <param name="tenantId">The tenant ID to set for the duration of the action.</param>
+        /// <param name="action">The action to execute.</param>
+        /// <param name="userId">Optional user ID to associate with the context.</param>
+        public static void ExecuteInTenantContext(this TenantContextHelper helper, string tenantId, Action action, string userId = null)
+        {
+            ArgumentNullException.ThrowIfNull(helper);
+            ArgumentNullException.ThrowIfNull(action);
+            ArgumentException.ThrowIfNullOrWhiteSpace(tenantId, nameof(tenantId));
+
+            using var scope = helper.CreateScope(tenantId, userId);
+            action();
+        }
+
+        /// <summary>
+        /// Executes a function within a tenant context scope, automatically restoring the previous context.
+        /// </summary>
+        /// <typeparam name="T">The return type of the function.</typeparam>
+        /// <param name="helper">The tenant context helper instance.</param>
+        /// <param name="tenantId">The tenant ID to set for the duration of the function.</param>
+        /// <param name="func">The function to execute.</param>
+        /// <param name="userId">Optional user ID to associate with the context.</param>
+        /// <returns>The result of the function execution.</returns>
+        public static T ExecuteInTenantContext<T>(this TenantContextHelper helper, string tenantId, Func<T> func, string userId = null)
+        {
+            ArgumentNullException.ThrowIfNull(helper);
+            ArgumentNullException.ThrowIfNull(func);
+            ArgumentException.ThrowIfNullOrWhiteSpace(tenantId, nameof(tenantId));
+
+            using var scope = helper.CreateScope(tenantId, userId);
+            return func();
+        }
+
+        /// <summary>
+        /// Gets the current tenant context or throws if not available.
+        /// </summary>
+        /// <param name="helper">The tenant context helper instance.</param>
+        /// <returns>The current tenant context.</returns>
+        /// <exception cref="InvalidOperationException">Thrown when no tenant context is set.</exception>
+        public static TenantContext GetRequiredTenantContext(this TenantContextHelper helper)
+        {
+            ArgumentNullException.ThrowIfNull(helper);
+
+            var context = helper.GetTenantContext();
+            if (context is null)
+            {
+                throw new InvalidOperationException("Tenant context is not set. Call SetTenantContext first.");
+            }
+
+            return context;
+        }
+
+        /// <summary>
+        /// Checks if the current tenant context matches the expected tenant ID.
+        /// </summary>
+        /// <param name="helper">The tenant context helper instance.</param>
+        /// <param name="expectedTenantId">The tenant ID to compare against.</param>
+        /// <returns>True if the current tenant matches the expected tenant; otherwise false.</returns>
+        public static bool IsCurrentTenant(this TenantContextHelper helper, string expectedTenantId)
+        {
+            ArgumentNullException.ThrowIfNull(helper);
+            return string.Equals(helper.GetCurrentTenantId(), expectedTenantId, StringComparison.Ordinal);
+        }
+    }
+}
