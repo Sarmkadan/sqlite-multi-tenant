b8395ce70e46d9f0bcfaf998c81ed52d4acdd563
Vladyslav Zaiets <rutova2@gmail.com>
2021-09-28 12:13:50 +0300

    serialize TenantServiceJson properly

---
+++ src/Services/TenantServiceJsonExtensions.cs
@@ -0,0 +1,81 @@
+using System.Text.Json;
+using System.Text.Json.Serialization.Metadata;
+
+namespace SqliteMultiTenant.Services;
+
+/// <summary>
+/// Provides JSON serialization and deserialization extensions for <see cref="TenantService"/>.
+/// </summary>
+public static class TenantServiceJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false,
+        TypeInfoResolver = new DefaultJsonTypeInfoResolver(),
+    };
+
+    /// <summary>
+    /// Serializes a <see cref="TenantService"/> instance to a JSON string.
+    /// </summary>
+    /// <param name="value">The tenant service instance to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+    /// <returns>A JSON string representation of the tenant service.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+    public static string ToJson(this TenantService value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        return JsonSerializer.Serialize(value, indented ? GetIndentedOptions() : _jsonOptions);
+    }
+
+    /// <summary>
+    /// Deserializes a JSON string to a <see cref="TenantService"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>The deserialized tenant service instance, or null if the JSON is null or empty.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
+    /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
+    public static TenantService? FromJson(string json)
+    {
+        ArgumentNullException.ThrowIfNull(json);
+
+        if (string.IsNullOrWhiteSpace(json))
+        {
+            return null;
+        }
+
+        return JsonSerializer.Deserialize<TenantService>(json, _jsonOptions);
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string to a <see cref="TenantService"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">Receives the deserialized tenant service instance if successful.</param>
+    /// <returns>True if deserialization succeeded; otherwise, false.</returns>
+    public static bool TryFromJson(string json, out TenantService? value)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+
+        try
+        {
+            value = JsonSerializer.Deserialize<TenantService>(json, _jsonOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            value = null;
+            return false;
+        }
+    }
+
+    private static JsonSerializerOptions GetIndentedOptions()
+    {
+        var options = new JsonSerializerOptions(_jsonOptions)
+        {
+            WriteIndented = true,
+        };
+        return options;
+    }
+}
