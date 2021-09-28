894002f7a212a0b9bfcb606eb8d367e9a72dad9b
Vladyslav Zaiets <rutova2@gmail.com>
2021-09-28 13:20:55 +0300

    Add ConnectionManagerJson json helpers

---
+++ src/Database/ConnectionManagerJsonExtensions.cs
@@ -0,0 +1,81 @@
+#nullable enable
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace SqliteMultiTenant.Database
+{
+    /// <summary>
+    /// Provides System.Text.Json serialization extensions for <see cref="ConnectionManager"/>.
+    /// </summary>
+    public static class ConnectionManagerJsonExtensions
+    {
+        private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            WriteIndented = false,
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+            ReferenceHandler = ReferenceHandler.IgnoreCycles
+        };
+
+        /// <summary>
+        /// Serializes the <see cref="ConnectionManager"/> instance to a JSON string.
+        /// </summary>
+        /// <param name="value">The connection manager to serialize.</param>
+        /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+        /// <returns>A JSON string representation of the connection manager.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when value is null.</exception>
+        public static string ToJson(this ConnectionManager value, bool indented = false)
+        {
+            ArgumentNullException.ThrowIfNull(value);
+
+            var options = indented
+                ? new JsonSerializerOptions(_jsonOptions) { WriteIndented = true }
+                : _jsonOptions;
+
+            return JsonSerializer.Serialize(value, options);
+        }
+
+        /// <summary>
+        /// Deserializes a JSON string to a <see cref="ConnectionManager"/> instance.
+        /// </summary>
+        /// <param name="json">The JSON string to deserialize.</param>
+        /// <returns>A deserialized ConnectionManager instance, or null if the JSON is invalid.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when json is null.</exception>
+        /// <exception cref="JsonException">Thrown when the JSON is malformed or cannot be deserialized.</exception>
+        public static ConnectionManager? FromJson(string json)
+        {
+            ArgumentNullException.ThrowIfNull(json);
+
+            return JsonSerializer.Deserialize<ConnectionManager>(json, _jsonOptions);
+        }
+
+        /// <summary>
+        /// Attempts to deserialize a JSON string to a <see cref="ConnectionManager"/> instance.
+        /// </summary>
+        /// <param name="json">The JSON string to deserialize.</param>
+        /// <param name="value">Receives the deserialized ConnectionManager if successful, otherwise null.</param>
+        /// <returns>True if deserialization succeeded; otherwise, false.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when json is null.</exception>
+        public static bool TryFromJson(string json, out ConnectionManager? value)
+        {
+            ArgumentNullException.ThrowIfNull(json);
+
+            try
+            {
+                value = JsonSerializer.Deserialize<ConnectionManager>(json, _jsonOptions);
+                return true;
+            }
+            catch (JsonException)
+            {
+                value = null;
+                return false;
+            }
+        }
+    }
+}
