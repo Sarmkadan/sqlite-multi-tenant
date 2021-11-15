32b9722ab5d86dec9ad5abe841d693a82f718f1f
Vladyslav Zaiets <rutova2@gmail.com>
2021-11-15 13:06:32 +0200

    feat: more BackupException extension methods
    one-liner really but wanted a clean commit.

--- src/Exceptions/BackupExceptionExtensions.cs
+++ src/Exceptions/BackupExceptionExtensions.cs
@@ -1,9 +1,55 @@
 namespace SqliteMultiTenant.Exceptions;
 
+/// <summary>
+/// Provides extension methods for <see cref="BackupException"/> to facilitate error classification and diagnostics.
+/// </summary>
 public static class BackupExceptionExtensions
 {
-    public static bool IsCreationFailure(this BackupException ex) => ex.Message.Contains("creation", StringComparison.OrdinalIgnoreCase);
-    public static bool IsVerificationFailure(this BackupException ex) => ex.Message.Contains("verification", StringComparison.OrdinalIgnoreCase);
-    public static bool IsRestoreFailure(this BackupException ex) => ex.Message.Contains("restore", StringComparison.OrdinalIgnoreCase);
-    public static string GetErrorDetails(this BackupException ex) => $"BackupId: {ex.BackupId}, DatabaseId: {ex.DatabaseId}, Message: {ex.Message}";
+    /// <summary>
+    /// Determines whether the exception represents a backup creation failure.
+    /// </summary>
+    /// <param name="ex">The backup exception to analyze. Must not be null.</param>
+    /// <returns>True if the exception message indicates a creation failure; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="ex"/> is null.</exception>
+    public static bool IsCreationFailure(this BackupException ex)
+    {
+        ArgumentNullException.ThrowIfNull(ex);
+        return ex.Message.Contains("creation", StringComparison.OrdinalIgnoreCase);
+    }
+
+    /// <summary>
+    /// Determines whether the exception represents a backup verification failure.
+    /// </summary>
+    /// <param name="ex">The backup exception to analyze. Must not be null.</param>
+    /// <returns>True if the exception message indicates a verification failure; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="ex"/> is null.</exception>
+    public static bool IsVerificationFailure(this BackupException ex)
+    {
+        ArgumentNullException.ThrowIfNull(ex);
+        return ex.Message.Contains("verification", StringComparison.OrdinalIgnoreCase);
+    }
+
+    /// <summary>
+    /// Determines whether the exception represents a backup restore failure.
+    /// </summary>
+    /// <param name="ex">The backup exception to analyze. Must not be null.</param>
+    /// <returns>True if the exception message indicates a restore failure; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="ex"/> is null.</exception>
+    public static bool IsRestoreFailure(this BackupException ex)
+    {
+        ArgumentNullException.ThrowIfNull(ex);
+        return ex.Message.Contains("restore", StringComparison.OrdinalIgnoreCase);
+    }
+
+    /// <summary>
+    /// Formats detailed error information from the backup exception.
+    /// </summary>
+    /// <param name="ex">The backup exception containing error details. Must not be null.</param>
+    /// <returns>A formatted string containing backup ID, database ID, and error message.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="ex"/> is null.</exception>
+    public static string GetErrorDetails(this BackupException ex)
+    {
+        ArgumentNullException.ThrowIfNull(ex);
+        return $"BackupId: {ex.BackupId}, DatabaseId: {ex.DatabaseId}, Message: {ex.Message}";
+    }
 }
