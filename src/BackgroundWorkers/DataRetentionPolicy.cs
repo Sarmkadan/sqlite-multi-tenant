1621fd3b2109a0de80cb5c745e484e617d4053d0
Vladyslav Zaiets <rutova2@gmail.com>
2021-11-15 20:01:29 +0200

    Cleanup in DaatRetentionPolicy

--- src/BackgroundWorkers/DataRetentionPolicy.cs
+++ src/BackgroundWorkers/DataRetentionPolicy.cs
@@ -12,17 +12,28 @@ using Microsoft.Extensions.Logging;
 
 namespace SqliteMultiTenant.BackgroundWorkers
 {
-    // Implements data retention policies for managing old records
-    // Automatically archives or deletes data based on configurable rules
+    /// <summary>
+    /// Implements data retention policies for managing old records.
+    /// Automatically archives or deletes data based on configurable rules.
+    /// </summary>
     public sealed class DataRetentionPolicy {
         private readonly ILogger<DataRetentionPolicy> _logger;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DataRetentionPolicy"/> class.
+        /// </summary>
+        /// <param name="logger">The logger instance.</param>
         public DataRetentionPolicy(ILogger<DataRetentionPolicy> logger)
         {
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
-        // Applies retention policy to a tenant's database
+        /// <summary>
+        /// Applies retention policy to a tenant's database.
+        /// </summary>
+        /// <param name="connection">The SQLite connection to the database.</param>
+        /// <param name="policy">The retention policy configuration.</param>
+        /// <returns>A <see cref="RetentionResult"/> containing the execution result.</returns>
         public async Task<RetentionResult> ApplyRetentionPolicyAsync(SQLiteConnection connection,
             RetentionPolicyConfig policy)
         {
@@ -60,7 +71,12 @@ namespace SqliteMultiTenant.BackgroundWorkers
             return result;
         }
 
-        // Applies a single retention rule
+        /// <summary>
+        /// Applies a single retention rule.
+        /// </summary>
+        /// <param name="connection">The SQLite connection to the database.</param>
+        /// <param name="rule">The retention rule to apply.</param>
+        /// <returns>A <see cref="RuleExecutionResult"/> containing the execution result.</returns>
         private async Task<RuleExecutionResult> ApplyRetentionRuleAsync(SQLiteConnection connection,
             RetentionRule rule)
         {
@@ -124,7 +140,11 @@ namespace SqliteMultiTenant.BackgroundWorkers
             return result;
         }
 
-        // Gets current retention configuration
+        /// <summary>
+        /// Gets the current retention configuration.
+        /// </summary>
+        /// <param name="tenantId">The tenant ID.</param>
+        /// <returns>A <see cref="RetentionPolicyConfig"/> containing the default policy configuration.</returns>
         public RetentionPolicyConfig GetDefaultPolicy(string tenantId)
         {
             return new RetentionPolicyConfig
@@ -146,42 +166,123 @@ namespace SqliteMultiTenant.BackgroundWorkers
         }
     }
 
+    /// <summary>
+    /// Represents a retention policy configuration.
+    /// </summary>
     public sealed class RetentionPolicyConfig {
+        /// <summary>
+        /// Gets or sets the tenant ID.
+        /// </summary>
         public string TenantId { get; set; }
+        /// <summary>
+        /// Gets or sets the list of retention rules.
+        /// </summary>
         public List<RetentionRule> Rules { get; set; } = new List<RetentionRule>();
+        /// <summary>
+        /// Gets or sets a value indicating whether to auto-execute the policy.
+        /// </summary>
         public bool AutoExecute { get; set; }
     }
 
+    /// <summary>
+    /// Represents a retention rule.
+    /// </summary>
     public sealed class RetentionRule {
+        /// <summary>
+        /// Gets or sets the table name.
+        /// </summary>
         public string TableName { get; set; }
+        /// <summary>
+        /// Gets or sets the date column name.
+        /// </summary>
         public string DateColumn { get; set; }
+        /// <summary>
+        /// Gets or sets the retention type.
+        /// </summary>
         public RetentionType RetentionType { get; set; }
+        /// <summary>
+        /// Gets or sets the retention value.
+        /// </summary>
         public int RetentionValue { get; set; }
+        /// <summary>
+        /// Gets or sets a value indicating whether the rule is enabled.
+        /// </summary>
         public bool IsEnabled { get; set; }
+        /// <summary>
+        /// Gets or sets a value indicating whether to archive before deleting.
+        /// </summary>
         public bool ArchiveBeforeDelete { get; set; }
+        /// <summary>
+        /// Gets or sets the archive table name.
+        /// </summary>
         public string ArchiveTableName { get; set; }
     }
 
+    /// <summary>
+    /// Represents a retention type.
+    /// </summary>
     public enum RetentionType
     {
+        /// <summary>
+        /// Days old.
+        /// </summary>
         DaysOld,
+        /// <summary>
+        /// Months old.
+        /// </summary>
         MonthsOld,
+        /// <summary>
+        /// Years old.
+        /// </summary>
         YearsOld
     }
 
+    /// <summary>
+    /// Represents a retention result.
+    /// </summary>
     public sealed class RetentionResult {
+        /// <summary>
+        /// Gets or sets a value indicating whether the policy was executed successfully.
+        /// </summary>
         public bool IsSuccessful { get; set; }
+        /// <summary>
+        /// Gets or sets the total number of records deleted.
+        /// </summary>
         public int TotalRecordsDeleted { get; set; }
+        /// <summary>
+        /// Gets or sets the execution date and time.
+        /// </summary>
         public DateTime ExecutedAt { get; set; }
+        /// <summary>
+        /// Gets or sets the error message (if any).
+        /// </summary>
         public string Error { get; set; }
+        /// <summary>
+        /// Gets or sets the dictionary of processed rules.
+        /// </summary>
         public Dictionary<string, RuleExecutionResult> ProcessedRules { get; set; } =
             new Dictionary<string, RuleExecutionResult>();
     }
 
+    /// <summary>
+    /// Represents a rule execution result.
+    /// </summary>
     public sealed class RuleExecutionResult {
+        /// <summary>
+        /// Gets or sets the table name.
+        /// </summary>
         public string TableName { get; set; }
+        /// <summary>
+        /// Gets or sets the number of records deleted.
+        /// </summary>
         public int RecordsDeleted { get; set; }
+        /// <summary>
+        /// Gets or sets the status message.
+        /// </summary>
         public string Status { get; set; }
+        /// <summary>
+        /// Gets or sets the cutoff date (if any).
+        /// </summary>
         public DateTime? CutoffDate { get; set; }
     }
 }
