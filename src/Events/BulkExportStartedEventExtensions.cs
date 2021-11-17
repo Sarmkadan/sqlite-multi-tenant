41e4181302741ddfd4f698444d0301afbf0865f9
Vladyslav Zaiets <rutova2@gmail.com>
2021-11-17 13:51:03 +0200

    Extension methods on BulkExportStartedEvent

--- src/Events/BulkExportStartedEventExtensions.cs
+++ src/Events/BulkExportStartedEventExtensions.cs
@@ -3,19 +3,38 @@ using System;
 
 namespace SqliteMultiTenant.Events
 {
+    /// <summary>
+    /// Provides extension methods for <see cref="BulkExportStartedEvent"/> instances.
+    /// Enables validation and formatting of bulk export operation events.
+    /// </summary>
     public static class BulkExportStartedEventExtensions
     {
+        /// <summary>
+        /// Determines whether the bulk export operation represented by the event is valid.
+        /// Validates that all required properties are non-null and non-empty.
+        /// </summary>
+        /// <param name="event">The event to validate.</param>
+        /// <returns>True if the event represents a valid operation; otherwise, false.</returns>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="event"/> is null.</exception>
         public static bool IsValidOperation(this BulkExportStartedEvent @event)
         {
-            return !string.IsNullOrEmpty(@event.DatabaseId) 
-                && @event.TableNames.Count > 0 
-                && !string.IsNullOrEmpty(@event.Format) 
-                && !string.IsNullOrEmpty(@event.OperationId);
+            ArgumentNullException.ThrowIfNull(@event);
+
+            return !string.IsNullOrWhiteSpace(@event.DatabaseId)
+                && @event.TableNames is not null
+                && @event.TableNames.Count > 0
+                && @event.TableNames.All(t => !string.IsNullOrWhiteSpace(t))
+                && !string.IsNullOrWhiteSpace(@event.Format)
+                && !string.IsNullOrWhiteSpace(@event.OperationId);
         }
 
-        public static string GetOperationSummary(this BulkExportStartedEvent @event)
-        {
-            return $"Exporting from database { @event.DatabaseId } to format { @event.Format } for tables { string.Join(", ", @event.TableNames) }";
-        }
+        /// <summary>
+        /// Generates a human-readable summary of the bulk export operation.
+        /// </summary>
+        /// <param name="event">The event containing export operation details.</param>
+        /// <returns>A formatted string describing the export operation.</returns>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="event"/> is null.</exception>
+        public static string GetOperationSummary(this BulkExportStartedEvent @event) =>
+            $"Exporting from database '{@event.DatabaseId}' to format '{@event.Format}' for tables [{string.Join(", ", @event.TableNames)}]";
     }
 }
