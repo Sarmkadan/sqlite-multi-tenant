e238afe8afc4b61e5d5013939a1cf03af10c9e46
Vladyslav Zaiets <rutova2@gmail.com>
2021-11-30 13:47:14 +0200

    feat(sqlite-multi-tenant.tests): more EventBusImpl extension methods

--- tests/sqlite-multi-tenant.Tests/EventBusImplTestsExtensions.cs
+++ tests/sqlite-multi-tenant.Tests/EventBusImplTestsExtensions.cs
@@ -14,8 +14,11 @@ namespace SqliteMultiTenant.Tests
         /// Ensures the event history is cleared, providing a clean state for subsequent tests.
         /// </summary>
         /// <param name="test">The <see cref="EventBusImplTests"/> instance.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="test"/> is <see langword="null"/>.</exception>
         public static void EnsureCleanState(this EventBusImplTests test)
         {
+            ArgumentNullException.ThrowIfNull(test);
+
             // Clears any existing events to start from a known state.
             test.ClearHistory_WhenHasEvents_ShouldClearList();
         }
@@ -24,8 +27,11 @@ namespace SqliteMultiTenant.Tests
         /// Verifies that the event history is empty initially.
         /// </summary>
         /// <param name="test">The <see cref="EventBusImplTests"/> instance.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="test"/> is <see langword="null"/>.</exception>
         public static void VerifyEmptyHistory(this EventBusImplTests test)
         {
+            ArgumentNullException.ThrowIfNull(test);
+
             // Asserts that the event history is empty at the start of the test suite.
             test.GetEventHistory_Initially_ShouldBeEmpty();
         }
@@ -34,8 +40,11 @@ namespace SqliteMultiTenant.Tests
         /// Runs the initialization test to ensure the event bus properties are correctly set.
         /// </summary>
         /// <param name="test">The <see cref="EventBusImplTests"/> instance.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="test"/> is <see langword="null"/>.</exception>
         public static void InitializeAndValidate(this EventBusImplTests test)
         {
+            ArgumentNullException.ThrowIfNull(test);
+
             // Executes the initialization verification.
             test.EventBus_Initialization_PropertiesAreSet();
         }
@@ -44,8 +53,11 @@ namespace SqliteMultiTenant.Tests
         /// Calls <see cref="EventBusImplTests.Dispose_WhenCalled_DoesNotThrow"/> to safely dispose the test fixture.
         /// </summary>
         /// <param name="test">The <see cref="EventBusImplTests"/> instance.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="test"/> is <see langword="null"/>.</exception>
         public static void DisposeSafely(this EventBusImplTests test)
         {
+            ArgumentNullException.ThrowIfNull(test);
+
             // Ensures Dispose does not throw any exceptions.
             test.Dispose_WhenCalled_DoesNotThrow();
         }
