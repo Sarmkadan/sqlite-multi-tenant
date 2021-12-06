7c7a10e38f12fd4cbaef014587ba82f5e5c356d9
Vladyslav Zaiets <rutova2@gmail.com>
2021-12-06 08:07:11 +0200

    small fix

--- src/Exceptions/MigrationExceptionExtensions.cs
+++ src/Exceptions/MigrationExceptionExtensions.cs
@@ -2,6 +2,7 @@ namespace SqliteMultiTenant.Exceptions;
 
 /// <summary>
 /// Provides extension methods for <see cref="MigrationException"/> to facilitate exception analysis and handling.
+/// These methods enable checking specific migration failure conditions and extracting migration metadata.
 /// </summary>
 public static class MigrationExceptionExtensions
 {
@@ -9,7 +10,7 @@ public static class MigrationExceptionExtensions
     /// Determines whether the exception represents an execution failure.
     /// </summary>
     /// <param name="exception">The exception to analyze. Cannot be null.</param>
-    /// <returns>True if the exception message indicates an execution failure; otherwise, false.</returns>
+    /// <returns>True if the exception message contains "execution failed" (case-insensitive); otherwise, false.</returns>
     /// <exception cref="ArgumentNullException">Thrown if <paramref name="exception"/> is null.</exception>
     public static bool IsExecutionFailure(this MigrationException exception)
     {
@@ -21,7 +22,7 @@ public static class MigrationExceptionExtensions
     /// Determines whether the exception represents a migration that was already applied.
     /// </summary>
     /// <param name="exception">The exception to analyze. Cannot be null.</param>
-    /// <returns>True if the exception message indicates the migration was already applied; otherwise, false.</returns>
+    /// <returns>True if the exception message contains "already applied" (case-insensitive); otherwise, false.</returns>
     /// <exception cref="ArgumentNullException">Thrown if <paramref name="exception"/> is null.</exception>
     public static bool IsVersionAlreadyApplied(this MigrationException exception)
     {
@@ -33,7 +34,11 @@ public static class MigrationExceptionExtensions
     /// Gets a formatted string containing migration details including ID and version.
     /// </summary>
     /// <param name="exception">The exception containing migration details. Cannot be null.</param>
-    /// <returns>A formatted string with migration details.</returns>
+    /// <returns>
+    /// A string in the format "Migration ID: {MigrationId}, Version: {MigrationVersion}".
+    /// Null values for <see cref="MigrationException.MigrationId"/> or <see cref="MigrationException.MigrationVersion"/>
+    /// are represented as "null" in the output.
+    /// </returns>
     /// <exception cref="ArgumentNullException">Thrown if <paramref name="exception"/> is null.</exception>
     public static string GetMigrationDetails(this MigrationException exception)
     {
