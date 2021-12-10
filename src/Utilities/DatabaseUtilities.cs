1d2cea21dec269663c22b5e478d9dcf3e7e101b1
Vladyslav Zaiets <rutova2@gmail.com>
2021-12-10 13:09:42 +0200

    tweak DatabaseUtilities

--- src/Utilities/DatabaseUtilities.cs
+++ src/Utilities/DatabaseUtilities.cs
@@ -12,10 +12,20 @@ using Microsoft.Extensions.Logging;
 
 namespace SqliteMultiTenant.Utilities
 {
-    // Common database utility functions for SQLite operations
+    /// <summary>
+/// Provides common database utility functions for SQLite operations in a multi-tenant environment.
+/// Includes methods for configuration, performance analysis, database maintenance, and schema inspection.
+/// </summary>
     public class DatabaseUtilities
     {
-        // Enables all pragmas for optimal multi-tenant performance
+        /// <summary>
+        /// Configures optimal SQLite database settings for multi-tenant performance.
+        /// Enables foreign keys, WAL journal mode, sets appropriate synchronous level, cache size,
+        /// and other performance-related pragmas to optimize database operations in a multi-tenant environment.
+        /// </summary>
+        /// <param name="connection">The SQLite connection to configure. Must be open or will be opened automatically.</param>
+        /// <returns>A Task representing the asynchronous operation.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when connection is null.</exception>
         public static async Task ConfigureOptimalSettingsAsync(SQLiteConnection connection)
         {
             if (connection is null)
@@ -40,7 +50,12 @@ namespace SqliteMultiTenant.Utilities
             }
         }
 
-        // Gets database file size in bytes
+        /// <summary>
+        /// Gets the size of the SQLite database file in bytes.
+        /// </summary>
+        /// <param name="databasePath">The file path to the SQLite database.</param>
+        /// <returns>The size of the database file in bytes, or 0 if the file doesn't exist or an error occurs.</returns>
+        /// <exception cref="ArgumentException">Thrown when databasePath is null or empty.</exception>
         public static long GetDatabaseSize(string databasePath)
         {
             if (string.IsNullOrEmpty(databasePath))
@@ -57,14 +72,25 @@ namespace SqliteMultiTenant.Utilities
             }
         }
 
-        // Gets human-readable database size
+        /// <summary>
+        /// Gets a human-readable formatted size of the SQLite database file.
+        /// </summary>
+        /// <param name="databasePath">The file path to the SQLite database.</param>
+        /// <returns>A formatted string representing the database size (e.g., "2.5 MB", "1.2 GB").</returns>
+        /// <seealso cref="GetDatabaseSize"/>
         public static string GetDatabaseSizeFormatted(string databasePath)
         {
             var bytes = GetDatabaseSize(databasePath);
             return FormatBytes(bytes);
         }
 
-        // Compacts database by removing deleted space
+        /// <summary>
+        /// Compacts the SQLite database by removing unused space and reducing file size.
+        /// Executes the VACUUM command which rebuilds the database file, repacking it into a minimal amount of disk space.
+        /// </summary>
+        /// <param name="connection">The SQLite connection to the database to compact. Must be open.</param>
+        /// <returns>A Task representing the asynchronous operation.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when connection is null.</exception>
         public static async Task CompactDatabaseAsync(SQLiteConnection connection)
         {
             if (connection is null)
@@ -77,7 +103,16 @@ namespace SqliteMultiTenant.Utilities
             }
         }
 
-        // Analyzes query performance
+        /// <summary>
+        /// Analyzes the performance characteristics of a SQL query using SQLite's EXPLAIN QUERY PLAN.
+        /// Returns information about how the query will be executed, including which indexes will be used,
+        /// whether full table scans will occur, and the join strategy.
+        /// </summary>
+        /// <param name="connection">The SQLite connection to analyze the query against. Must be open.</param>
+        /// <param name="query">The SQL query to analyze.</param>
+        /// <returns>A Task representing the asynchronous operation.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when connection is null.</exception>
+        /// <exception cref="ArgumentException">Thrown when query is null or empty.</exception>
         public static async Task AnalyzeQueryPerformanceAsync(SQLiteConnection connection, string query)
         {
             if (connection is null)
@@ -100,7 +135,12 @@ namespace SqliteMultiTenant.Utilities
             }
         }
 
-        // Gets current database statistics
+        /// <summary>
+        /// Retrieves comprehensive statistics about the SQLite database.
+        /// Includes table count, index count, page count, page size, and estimated database size.
+        /// </summary>
+        /// <param name="connection">The SQLite connection to the database to analyze. Must be open.</param>
+        /// <returns>A <see cref="DatabaseStatistics"/> object containing database statistics, or a default instance if an error occurs.</returns>
         public static async Task<DatabaseStatistics> GetDatabaseStatisticsAsync(SQLiteConnection connection)
         {
             var stats = new DatabaseStatistics();
@@ -145,7 +185,12 @@ namespace SqliteMultiTenant.Utilities
             return stats;
         }
 
-        // Checks if a table exists
+        /// <summary>
+        /// Checks whether a specific table exists in the SQLite database.
+        /// </summary>
+        /// <param name="connection">The SQLite connection to check. Must be open.</param>
+        /// <param name="tableName">The name of the table to check for existence.</param>
+        /// <returns>True if the table exists; otherwise, false.</returns>
         public static async Task<bool> TableExistsAsync(SQLiteConnection connection, string tableName)
         {
             if (connection is null || string.IsNullOrEmpty(tableName))
@@ -169,7 +214,13 @@ namespace SqliteMultiTenant.Utilities
             }
         }
 
-        // Checks if a column exists in a table
+        /// <summary>
+        /// Checks whether a specific column exists in a table within the SQLite database.
+        /// </summary>
+        /// <param name="connection">The SQLite connection to check. Must be open.</param>
+        /// <param name="tableName">The name of the table to check.</param>
+        /// <param name="columnName">The name of the column to check for existence.</param>
+        /// <returns>True if the column exists in the specified table; otherwise, false.</returns>
         public static async Task<bool> ColumnExistsAsync(SQLiteConnection connection,
             string tableName, string columnName)
         {
@@ -197,7 +248,13 @@ namespace SqliteMultiTenant.Utilities
             return false;
         }
 
-        // Gets all columns for a table
+        /// <summary>
+        /// Retrieves information about all columns in a specified table.
+        /// Uses PRAGMA table_info to get column names, types, constraints, default values, and primary key information.
+        /// </summary>
+        /// <param name="connection">The SQLite connection to the database. Must be open.</param>
+        /// <param name="tableName">The name of the table to inspect.</param>
+        /// <returns>A list of <see cref="ColumnInfo"/> objects describing each column in the table, or an empty list if an error occurs.</returns>
         public static async Task<List<ColumnInfo>> GetTableColumnsAsync(SQLiteConnection connection,
             string tableName)
         {
@@ -249,19 +306,64 @@ namespace SqliteMultiTenant.Utilities
         }
     }
 
-    public sealed class DatabaseStatistics {
+    /// <summary>
+/// Represents comprehensive statistics about an SQLite database.
+/// </summary>
+public sealed class DatabaseStatistics {
+        /// <summary>
+        /// Gets or sets the number of tables in the database.
+        /// </summary>
         public long TableCount { get; set; }
+
+        /// <summary>
+        /// Gets or sets the number of indexes in the database.
+        /// </summary>
         public long IndexCount { get; set; }
+
+        /// <summary>
+        /// Gets or sets the number of pages in the database file.
+        /// </summary>
         public long PageCount { get; set; }
+
+        /// <summary>
+        /// Gets or sets the size of each page in bytes.
+        /// </summary>
         public long PageSize { get; set; }
+
+        /// <summary>
+        /// Gets or sets the estimated total size of the database in bytes.
+        /// Calculated as PageCount * PageSize.
+        /// </summary>
         public long EstimatedSize { get; set; }
     }
 
-    public sealed class ColumnInfo {
+    /// <summary>
+/// Represents information about a column in an SQLite table.
+/// </summary>
+public sealed class ColumnInfo {
+        /// <summary>
+        /// Gets or sets the name of the column.
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the data type of the column (e.g., "INTEGER", "TEXT", "BLOB").
+        /// </summary>
         public string Type { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the column is defined as NOT NULL.
+        /// </summary>
         public bool NotNull { get; set; }
+
+        /// <summary>
+        /// Gets or sets the default value of the column, or null if no default is defined.
+        /// </summary>
         public string DefaultValue { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the column is part of the primary key.
+        /// </summary>
         public bool PrimaryKey { get; set; }
     }
 }
