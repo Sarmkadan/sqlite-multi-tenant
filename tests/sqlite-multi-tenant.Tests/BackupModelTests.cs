a7a27433b1243f5f0cd9d5bc90a722c99cdb1ac6
Vladyslav Zaiets <rutova2@gmail.com>
2021-12-10 16:38:49 +0200

    refactor: cleanup in BackupModelTests

--- tests/sqlite-multi-tenant.Tests/BackupModelTests.cs
+++ tests/sqlite-multi-tenant.Tests/BackupModelTests.cs
@@ -9,12 +9,22 @@ using SqliteMultiTenant.Constants;
 using SqliteMultiTenant.Models;
 using Xunit;
 
-namespace SqliteMultiTenant.Tests;
-
-public sealed class BackupModelTests {
+/// <summary>
+/// Tests for the Backup class.
+/// </summary>
+public sealed class BackupModelTests
+{
+    /// <summary>
+    /// Creates a new Backup instance with the specified ID.
+    /// </summary>
+    /// <param name="id">The ID of the backup.</param>
+    /// <returns>A new Backup instance.</returns>
     private static Backup CreateBackup(string id = "bkp-001") =>
         new() { BackupId = id, DatabaseId = "db-001", BackupPath = $"/data/{id}.sqlite" };
 
+    /// <summary>
+    /// Tests that MarkAsCompleted correctly computes the compression ratio.
+    /// </summary>
     [Fact]
     public void MarkAsCompleted_WithOriginalSizeSet_ComputesCompressionRatio()
     {
@@ -34,6 +44,9 @@ public sealed class BackupModelTests {
         backup.ErrorMessage.Should().BeNull();
     }
 
+    /// <summary>
+    /// Tests that MarkAsFailed correctly sets the failed status and preserves the error message.
+    /// </summary>
     [Fact]
     public void MarkAsFailed_WithErrorMessage_SetsFailedStatusAndPreservesMessage()
     {
@@ -45,6 +58,9 @@ public sealed class BackupModelTests {
         backup.ErrorMessage.Should().Be("Disk quota exceeded");
     }
 
+    /// <summary>
+    /// Tests that AddTag correctly builds and parses a comma-delimited string of tags.
+    /// </summary>
     [Fact]
     public void AddTag_WithMultipleTagCalls_BuildsCommaDelimitedStringAndParsesBack()
     {
@@ -58,6 +74,9 @@ public sealed class BackupModelTests {
         backup.GetTags().Should().HaveCount(3).And.ContainInOrder("daily", "critical", "2024-q1");
     }
 
+    /// <summary>
+    /// Tests that IsExpired correctly returns false when the expiration date is in the future.
+    /// </summary>
     [Fact]
     public void IsExpired_WhenExpirationDateIsInFuture_ReturnsFalse()
     {
