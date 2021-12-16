10f6030a39cece9157064f7d64fbb42dfefd0b16
Vladyslav Zaiets <rutova2@gmail.com>
2021-12-16 13:00:22 +0200

    feat(sqlite-multi-tenant.tests): validate TenantNameValidator args.
    one-liner really but wanted a clean commit.

--- tests/sqlite-multi-tenant.Tests/TenantNameValidatorTests.cs
+++ tests/sqlite-multi-tenant.Tests/TenantNameValidatorTests.cs
@@ -10,41 +10,72 @@ using Xunit;
 
 namespace SqliteMultiTenant.Tests;
 
-public sealed class TenantNameValidatorTests {
-    [Fact]
-    public void ValidateTenantId_WithValidId_ReturnsValidResult()
-    {
-        var result = TenantNameValidator.ValidateTenantId("my-tenant");
+/// <summary>
+/// Contains unit tests for <see cref="TenantNameValidator"/> tenant name validation and generation functionality.
+/// </summary>
+public sealed class TenantNameValidatorTests
+{
+	/// <summary>
+	/// Validates that a properly formatted tenant ID returns a valid validation result.
+	/// </summary>
+	[Fact]
+	public void ValidateTenantId_WithValidId_ReturnsValidResult()
+	{
+		// Act
+		var result = TenantNameValidator.ValidateTenantId("my-tenant");
 
-        result.IsValid.Should().BeTrue();
-        result.Error.Should().BeNullOrEmpty();
-    }
+		// Assert
+		result.IsValid.Should().BeTrue();
+		result.Error.Should().BeNullOrEmpty();
+	}
 
-    [Fact]
-    public void ValidateTenantId_BelowMinimumLength_ReturnsInvalidWithLengthError()
-    {
-        // Minimum is 3 characters; "ab" is only 2
-        var result = TenantNameValidator.ValidateTenantId("ab");
+	/// <summary>
+	/// Validates that a tenant ID below the minimum length (3 characters) returns an invalid result with a length error.
+	/// </summary>
+	[Fact]
+	public void ValidateTenantId_BelowMinimumLength_ReturnsInvalidWithLengthError()
+	{
+		// Arrange
+		// Minimum is 3 characters; "ab" is only 2
 
-        result.IsValid.Should().BeFalse();
-        result.Error.Should().Contain("3");
-    }
+		// Act
+		var result = TenantNameValidator.ValidateTenantId("ab");
 
-    [Fact]
-    public void ValidateTenantId_UsingReservedWord_ReturnsReservedError()
-    {
-        var result = TenantNameValidator.ValidateTenantId("admin");
+		// Assert
+		result.IsValid.Should().BeFalse();
+		result.Error.Should().Contain("3");
+	}
 
-        result.IsValid.Should().BeFalse();
-        result.Error.Should().Contain("reserved");
-    }
+	/// <summary>
+	/// Validates that a tenant ID using a reserved word returns an invalid result with a reserved word error.
+	/// </summary>
+	[Fact]
+	public void ValidateTenantId_UsingReservedWord_ReturnsReservedError()
+	{
+		// Act
+		var result = TenantNameValidator.ValidateTenantId("admin");
 
-    [Fact]
-    public void GenerateTenantId_FromNameWithSpecialCharactersAndSpaces_ReturnsNormalizedSlug()
-    {
-        // Spaces become hyphens, "!" is stripped, everything is lowercased
-        var result = TenantNameValidator.GenerateTenantId("Acme Corporation 2024!");
+		// Assert
+		result.IsValid.Should().BeFalse();
+		result.Error.Should().Contain("reserved");
+	}
 
-        result.Should().Be("acme-corporation-2024");
-    }
+	/// <summary>
+	/// Validates that a tenant name with special characters and spaces is properly normalized to a tenant ID slug.
+	/// </summary>
+	/// <remarks>
+	/// The normalization process converts spaces to hyphens, removes special characters like '!', and converts to lowercase.
+	/// </remarks>
+	[Fact]
+	public void GenerateTenantId_FromNameWithSpecialCharactersAndSpaces_ReturnsNormalizedSlug()
+	{
+		// Arrange
+		// Spaces become hyphens, "!" is stripped, everything is lowercased
+
+		// Act
+		var result = TenantNameValidator.GenerateTenantId("Acme Corporation 2024!");
+
+		// Assert
+		result.Should().Be("acme-corporation-2024");
+	}
 }
