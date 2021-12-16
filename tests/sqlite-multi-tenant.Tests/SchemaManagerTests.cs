ee7f840c654c4a67a065884a34c59a2890e19119
Vladyslav Zaiets <rutova2@gmail.com>
2021-12-16 15:23:34 +0200

    small refactor - SchemaManagerTests
    also touched a couple of call sites.

--- tests/sqlite-multi-tenant.Tests/SchemaManagerTests.cs
+++ tests/sqlite-multi-tenant.Tests/SchemaManagerTests.cs
@@ -5,6 +5,15 @@
 // =============================================================================
 
 using FluentAssertions;
+
+/// <summary>
+/// Unit tests for <see cref="SchemaManager"/> class that verify schema initialization, table management,
+/// column operations, and index creation functionality for multi-tenant SQLite databases.
+/// </summary>
+/// <remarks>
+/// Tests use in-memory SQLite databases with shared cache to ensure isolation between test runs.
+/// Each test creates its own uniquely named database instance to prevent state leakage.
+/// </remarks>
 using NSubstitute;
 using Microsoft.Extensions.Logging;
 using SqliteMultiTenant.Database;
@@ -16,12 +25,28 @@ using System.Linq;
 
 namespace SqliteMultiTenant.Tests
 {
-    public sealed class SchemaManagerTests : IDisposable {
+    /// <summary>
+    /// Test suite for <see cref="SchemaManager"/> functionality including schema initialization, table operations,
+    /// column management, and index creation for multi-tenant SQLite databases.
+    /// </summary>
+    /// <remarks>
+    /// Implements <see cref="IDisposable"/> to properly clean up database connections after each test.
+    /// </remarks>
+    public sealed class SchemaManagerTests : IDisposable
+    {
         private readonly ILogger<SchemaManager> _mockLogger;
         private SQLiteConnection _connection;
         private string _connectionString;
         private SchemaManager _sut;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SchemaManagerTests"/> class.
+        /// </summary>
+        /// <remarks>
+        /// Each test instance gets its own uniquely named shared-cache memory database,
+        /// since System.Data.SQLite pools connections and a fixed name would leak state
+        /// (e.g. tables/indexes) across test methods.
+        /// </remarks>
         public SchemaManagerTests()
         {
             // Each test instance gets its own uniquely named shared-cache memory database,
@@ -36,6 +61,11 @@ namespace SqliteMultiTenant.Tests
             _connection.Open();
         }
 
+        /// <summary>
+        /// Determines whether a table with the specified name exists in the database.
+        /// </summary>
+        /// <param name="tableName">Name of the table to check.</param>
+        /// <returns><see langword="true"/> if the table exists; otherwise, <see langword="false"/>.</returns>
         private async Task<bool> TableExists(string tableName)
         {
             using (var command = _connection.CreateCommand())
@@ -46,6 +76,12 @@ namespace SqliteMultiTenant.Tests
             }
         }
 
+        /// <summary>
+        /// Determines whether a column with the specified name exists in the specified table.
+        /// </summary>
+        /// <param name="tableName">Name of the table to check.</param>
+        /// <param name="columnName">Name of the column to check.</param>
+        /// <returns><see langword="true"/> if the column exists; otherwise, <see langword="false"/>.</returns>
         private async Task<bool> ColumnExists(string tableName, string columnName)
         {
             using (var command = _connection.CreateCommand())
@@ -75,6 +111,10 @@ namespace SqliteMultiTenant.Tests
             }
         }
 
+        /// <summary>
+        /// Verifies that the <see cref="SchemaManager"/> constructor throws an <see cref="ArgumentNullException"/>
+        /// when a null logger is provided.
+        /// </summary>
         [Fact]
         public void SchemaManager_Constructor_ThrowsArgumentNullException_WhenLoggerIsNull()
         {
@@ -84,6 +124,10 @@ namespace SqliteMultiTenant.Tests
                 .WithParameterName("logger");
         }
 
+        /// <summary>
+        /// Verifies that the <see cref="SchemaManager"/> constructor throws an <see cref="ArgumentNullException"/>
+        /// when a null connection string is provided.
+        /// </summary>
         [Fact]
         public void SchemaManager_Constructor_ThrowsArgumentNullException_WhenConnectionStringIsNull()
         {
@@ -93,6 +137,10 @@ namespace SqliteMultiTenant.Tests
                 .WithParameterName("connectionString");
         }
 
+        /// <summary>
+        /// Verifies that calling <see cref="SchemaManager.InitializeSchemaAsync"/> creates the required tables and indexes
+        /// for a new tenant in the database.
+        /// </summary>
         [Fact]
         public async Task InitializeSchemaAsync_CreatesTablesAndIndexes()
         {
@@ -111,6 +159,10 @@ namespace SqliteMultiTenant.Tests
             _mockLogger.AssertLogged(LogLevel.Information, 1, "Schema initialized for tenant: {TenantId}", tenantId);
         }
 
+        /// <summary>
+        /// Verifies that calling <see cref="SchemaManager.InitializeSchemaAsync"/> multiple times does not recreate existing tables and indexes
+        /// and does not throw exceptions.
+        /// </summary>
         [Fact]
         public async Task InitializeSchemaAsync_ShouldNotRecreateExistingTablesAndIndexes()
         {
@@ -126,6 +178,10 @@ namespace SqliteMultiTenant.Tests
             _mockLogger.AssertLogged(LogLevel.Information, 2, "Schema initialized for tenant: {TenantId}", tenantId);
         }
 
+        /// <summary>
+        /// Verifies that calling <see cref="SchemaManager.AddColumnAsync"/> adds a new column to an existing table
+        /// in the database for the specified tenant.
+        /// </summary>
         [Fact]
         public async Task AddColumnAsync_AddsNewColumnToTable()
         {
@@ -146,6 +202,10 @@ namespace SqliteMultiTenant.Tests
                 columnName, tableName, tenantId);
         }
 
+        /// <summary>
+        /// Verifies that calling <see cref="SchemaManager.AddColumnAsync"/> returns false when attempting to add a column that already exists
+        /// in the specified table, without throwing an exception.
+        /// </summary>
         [Fact]
         public async Task AddColumnAsync_ReturnsFalse_WhenColumnAlreadyExists()
         {
@@ -165,6 +225,9 @@ namespace SqliteMultiTenant.Tests
                 columnName, tableName);
         }
 
+        /// <summary>
+        /// Verifies that calling <see cref="SchemaManager.AddColumnAsync"/> throws a <see cref="SQLiteException"/> when attempting to add a column to a non-existent table.
+        /// </summary>
         [Fact]
         public async Task AddColumnAsync_ThrowsExceptionAndLogs_WhenTableNameDoesNotExist()
         {
@@ -183,6 +246,9 @@ namespace SqliteMultiTenant.Tests
                 "Failed to add column {ColumnName} to table {TableName}", columnName, nonExistentTable);
         }
 
+        /// <summary>
+        /// Verifies that calling <see cref="SchemaManager.RenameTableAsync"/> successfully renames an existing table in the database.
+        /// </summary>
         [Fact]
         public async Task RenameTableAsync_RenamesTableSuccessfully()
         {
@@ -198,9 +264,13 @@ namespace SqliteMultiTenant.Tests
             // Assert
             (await TableExists(oldName)).Should().BeFalse();
             (await TableExists(newName)).Should().BeTrue();
-            _mockLogger.AssertLogged(LogLevel.Information, 1, "Table renamed from {OldName} to {NewName}", oldName, newName);
+            _mockLogger.AssertLogged(LogLevel.Information, 1, "Table renamed from {OldName} to {NewName}",
+                oldName, newName);
         }
 
+        /// <summary>
+        /// Verifies that calling <see cref="SchemaManager.RenameTableAsync"/> throws a <see cref="SQLiteException"/> when attempting to rename a non-existent table.
+        /// </summary>
         [Fact]
         public async Task RenameTableAsync_ThrowsExceptionAndLogs_WhenOldTableDoesNotExist()
         {
@@ -217,6 +287,9 @@ namespace SqliteMultiTenant.Tests
                 "Failed to rename table from {OldName} to {NewName}", nonExistentTable, newName);
         }
 
+        /// <summary>
+        /// Verifies that calling <see cref="SchemaManager.CreateIndexAsync"/> creates a new index on the specified table with the given column names.
+        /// </summary>
         [Fact]
         public async Task CreateIndexAsync_CreatesNewIndex()
         {
@@ -233,9 +306,14 @@ namespace SqliteMultiTenant.Tests
             // Assert
             result.Should().BeTrue();
             (await IndexExists(indexName)).Should().BeTrue();
-            _mockLogger.AssertLogged(LogLevel.Information, 1, "Index {IndexName} created on table {TableName}", indexName, tableName);
+            _mockLogger.AssertLogged(LogLevel.Information, 1, "Index {IndexName} created on table {TableName}",
+                indexName, tableName);
         }
 
+        /// <summary>
+        /// Verifies that calling <see cref="SchemaManager.CreateIndexAsync"/> returns false when attempting to create an index that already exists,
+        /// without throwing an exception.
+        /// </summary>
         [Fact]
         public async Task CreateIndexAsync_ReturnsFalse_WhenIndexAlreadyExists()
         {
@@ -254,6 +332,10 @@ namespace SqliteMultiTenant.Tests
             _mockLogger.AssertLogged(LogLevel.Warning, 1, "Index {IndexName} already exists", indexName);
         }
 
+        /// <summary>
+        /// Verifies that calling <see cref="SchemaManager.GetTablesAsync"/> returns all user-created tables in the database,
+        /// excluding system tables like sqlite_sequence.
+        /// </summary>
         [Fact]
         public async Task GetTablesAsync_ReturnsAllUserTables()
         {
@@ -271,6 +353,9 @@ namespace SqliteMultiTenant.Tests
             tables.Should().HaveCount(2);
         }
 
+        /// <summary>
+        /// Verifies that calling <see cref="SchemaManager.GetTablesAsync"/> returns an empty list when no tables exist in the database.
+        /// </summary>
         [Fact]
         public async Task GetTablesAsync_ReturnsEmptyList_WhenNoTablesExist()
         {
@@ -285,7 +370,9 @@ namespace SqliteMultiTenant.Tests
             tables.Should().BeEmpty();
         }
 
-
+        /// <summary>
+        /// Releases all resources used by the current test instance, including database connections.
+        /// </summary>
         public void Dispose()
         {
             _connection?.Close();
