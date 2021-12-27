a03e4c6f67bc5a668d769825a03f149e5e658c86
Vladyslav Zaiets <rutova2@gmail.com>
2021-12-27 17:22:22 +0200

    guard clauses in OldMultiTenantOptionsValidation
    found this while looking at something else.

--- tests/sqlite-multi-tenant.Tests/OldMultiTenantOptionsValidationTests.cs
+++ tests/sqlite-multi-tenant.Tests/OldMultiTenantOptionsValidationTests.cs
@@ -11,7 +11,17 @@ using Xunit;
 
 namespace SqliteMultiTenant.Tests
 {
+    /// <summary>
+    /// Contains unit tests that verify the validation logic of <see cref="MultiTenantOptions"/>
+    /// and related option classes (<see cref="BackupOptions"/>, <see cref="SecurityOptions"/>).
+    /// Each test exercises a specific validation rule and asserts that the expected
+    /// exception is thrown (or not thrown) by <see cref="OptionsValidator.Validate"/>.
+    /// </summary>
     public sealed class MultiTenantOptionsValidationTests {
+        /// <summary>
+        /// Verifies that <see cref="OptionsValidator.Validate(MultiTenantOptions)"/> does not
+        /// throw an exception when all required properties contain valid values.
+        /// </summary>
         [Fact]
         public void Validate_MultiTenantOptions_ShouldNotThrowException_WithValidOptions()
         {
@@ -31,6 +41,10 @@ namespace SqliteMultiTenant.Tests
             act.Should().NotThrow();
         }
 
+        /// <summary>
+        /// Verifies that validation throws an <see cref="ArgumentException"/> when
+        /// <see cref="MultiTenantOptions.BasePath"/> is an empty string.
+        /// </summary>
         [Fact]
         public void Validate_MultiTenantOptions_ShouldThrowArgumentException_WhenBasePathIsEmpty()
         {
@@ -51,6 +65,10 @@ namespace SqliteMultiTenant.Tests
                 .WithMessage("BasePath cannot be empty");
         }
 
+        /// <summary>
+        /// Verifies that validation throws an <see cref="ArgumentException"/> when
+        /// <see cref="MultiTenantOptions.MaxConnectionsPerTenant"/> is zero or less.
+        /// </summary>
         [Fact]
         public void Validate_MultiTenantOptions_ShouldThrowArgumentException_WhenMaxConnectionsPerTenantIsZeroOrLess()
         {
@@ -71,6 +89,10 @@ namespace SqliteMultiTenant.Tests
                 .WithMessage("MaxConnectionsPerTenant must be greater than 0");
         }
 
+        /// <summary>
+        /// Verifies that validation throws an <see cref="ArgumentException"/> when
+        /// <see cref="MultiTenantOptions.MaxBackupCount"/> is zero or less.
+        /// </summary>
         [Fact]
         public void Validate_MultiTenantOptions_ShouldThrowArgumentException_WhenMaxBackupCountIsZeroOrLess()
         {
@@ -91,6 +113,10 @@ namespace SqliteMultiTenant.Tests
                 .WithMessage("MaxBackupCount must be greater than 0");
         }
 
+        /// <summary>
+        /// Verifies that validation throws an <see cref="ArgumentException"/> when
+        /// <see cref="MultiTenantOptions.BackupRetention"/> is zero or less.
+        /// </summary>
         [Fact]
         public void Validate_MultiTenantOptions_ShouldThrowArgumentException_WhenBackupRetentionIsZeroOrLess()
         {
@@ -111,6 +137,10 @@ namespace SqliteMultiTenant.Tests
                 .WithMessage("BackupRetention must be positive");
         }
 
+        /// <summary>
+        /// Verifies that <see cref="OptionsValidator.Validate(BackupOptions)"/> does not
+        /// throw an exception when the backup options are valid.
+        /// </summary>
         [Fact]
         public void Validate_BackupOptions_ShouldNotThrowException_WithValidOptions()
         {
@@ -128,6 +158,10 @@ namespace SqliteMultiTenant.Tests
             act.Should().NotThrow();
         }
 
+        /// <summary>
+        /// Verifies that validation throws an <see cref="ArgumentException"/> when
+        /// <see cref="BackupOptions.MaxConcurrentBackups"/> is zero or less.
+        /// </summary>
         [Fact]
         public void Validate_BackupOptions_ShouldThrowArgumentException_WhenMaxConcurrentBackupsIsZeroOrLess()
         {
@@ -146,6 +180,10 @@ namespace SqliteMultiTenant.Tests
                 .WithMessage("MaxConcurrentBackups must be greater than 0");
         }
 
+        /// <summary>
+        /// Verifies that validation throws an <see cref="ArgumentException"/> when
+        /// <see cref="BackupOptions.BackupTimeoutSeconds"/> is zero or less.
+        /// </summary>
         [Fact]
         public void Validate_BackupOptions_ShouldThrowArgumentException_WhenBackupTimeoutSecondsIsZeroOrLess()
         {
@@ -164,6 +202,10 @@ namespace SqliteMultiTenant.Tests
                 .WithMessage("BackupTimeoutSeconds must be greater than 0");
         }
 
+        /// <summary>
+        /// Verifies that <see cref="OptionsValidator.Validate(SecurityOptions)"/> does not
+        /// throw an exception when all security options are valid.
+        /// </summary>
         [Fact]
         public void Validate_SecurityOptions_ShouldNotThrowException_WithValidOptions()
         {
@@ -182,6 +224,10 @@ namespace SqliteMultiTenant.Tests
             act.Should().NotThrow();
         }
 
+        /// <summary>
+        /// Verifies that validation throws an <see cref="ArgumentException"/> when
+        /// <see cref="SecurityOptions.SessionTimeout"/> is zero or less.
+        /// </summary>
         [Fact]
         public void Validate_SecurityOptions_ShouldThrowArgumentException_WhenSessionTimeoutIsZeroOrLess()
         {
@@ -201,6 +247,10 @@ namespace SqliteMultiTenant.Tests
                 .WithMessage("SessionTimeout must be positive");
         }
 
+        /// <summary>
+        /// Verifies that validation throws an <see cref="ArgumentException"/> when
+        /// <see cref="SecurityOptions.MaxFailedLoginAttempts"/> is zero or less.
+        /// </summary>
         [Fact]
         public void Validate_SecurityOptions_ShouldThrowArgumentException_WhenMaxFailedLoginAttemptsIsZeroOrLess()
         {
@@ -220,6 +270,10 @@ namespace SqliteMultiTenant.Tests
                 .WithMessage("MaxFailedLoginAttempts must be greater than 0");
         }
 
+        /// <summary>
+        /// Verifies that validation throws an <see cref="ArgumentException"/> when
+        /// <see cref="SecurityOptions.LockoutDuration"/> is zero or less.
+        /// </summary>
         [Fact]
         public void Validate_SecurityOptions_ShouldThrowArgumentException_WhenLockoutDurationIsZeroOrLess()
         {
