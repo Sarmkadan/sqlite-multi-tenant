d78f496b4a19aa4e30f4ec37749ba789bd2eb445
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-22 21:28:19 +0300

    dedupe some logic in IntegrityCheckServiceExtensions

--- src/Services/IntegrityCheckServiceExtensions.cs
+++ src/Services/IntegrityCheckServiceExtensions.cs
@@ -10,20 +10,20 @@ using SqliteMultiTenant.Services;
 namespace SqliteMultiTenant.Services;
 
 /// <summary>
-/// Extension methods for registering IntegrityCheckService with dependency injection.
+/// Extension methods for registering <see cref="IIntegrityCheckService"/> with dependency injection.
 /// </summary>
 public static class IntegrityCheckServiceExtensions
 {
     /// <summary>
     /// Adds the integrity check service to the service collection.
     /// </summary>
-    /// <param name="services">The service collection.</param>
-    /// <returns>The service collection.</returns>
+    /// <param name="services">The service collection to add services to.</param>
+    /// <returns>The same service collection so that multiple calls can be chained.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="services"/> is <see langword="null"/>.</exception>
     public static IServiceCollection AddIntegrityCheckService(
         this IServiceCollection services)
     {
-        if (services is null)
-            throw new ArgumentNullException(nameof(services));
+        ArgumentNullException.ThrowIfNull(services);
 
         services.AddScoped<IIntegrityCheckService, IntegrityCheckService>();
 
