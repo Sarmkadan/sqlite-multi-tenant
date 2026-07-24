df595757cb43d61aafa05d6d8bcae3ec791047e5
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-24 21:30:00 +0300

    chore: update TenantStorageInfoJsonExtensionsTests
    one-liner really but wanted a clean commit.

---
+++ tests/sqlite-multi-tenant.Tests/TenantStorageInfoJsonExtensionsTests.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Diagnostics;
+using System.Text.Json;
+using Xunit;
+using sqlite_multi_tenant.Tests;
+
+namespace sqlite_multi_tenant.Tests
+{
+    public class TenantStorageInfoJsonExtensionsTests
+    {
+        [Fact]
+        public void ToJson_Happy_PATH()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void FromJson_HAPPY_PATH()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void TryFromJson_NULL_INPUT()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void TryFromJson_EMPTY_COLLECTION()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void TryFromJson_BOUNDARY_VALUES()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void TryFromJson_ERROR_PATH()
+        {
+            // Test implementation here
+        }
+    }
+}
