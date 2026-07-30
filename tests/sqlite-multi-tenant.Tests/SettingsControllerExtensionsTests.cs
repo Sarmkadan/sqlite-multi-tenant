4470b199c923559452fae9c13cf012be4b51b816
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-30 19:43:42 +0300

    chore: SettingsControllerExtensionsTests changes

---
+++ tests/sqlite-multi-tenant.Tests/SettingsControllerExtensionsTests.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Microsoft.AspNetCore.Mvc;
+using SqliteMultiTenant.Api.Controllers;
+using SqliteMultiTenant.Api.Responses;
+using Xunit;
+
+namespace SqliteMultiTenant.Tests
+{
+    public class SettingsControllerExtensionsTests
+    {
+        // ... entire file content ...
+    }
+}
