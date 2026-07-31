1d04f31c170281cf92a80d3e13cc12dee88d9ecd
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-31 21:30:00 +0300

    JsonHelperTetss.cs

---
+++ tests/sqlite-multi-tenant.Tests/JsonHelperTests.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace sqlite_multi_tenant.Tests
+{
+    public class JsonHelperTests
+    {
+        public JsonHelperTests()
+        {
+        }
+
+        [Fact]
+        public void Serialize_Happy_Path_Test()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void Deserialize_Happy_Path_Test()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void DeserializeDynamic_Happy_Path_Test()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void MergeJson_Happy_Path_Test()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void GetProperty_Happy_Path_Test()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void IsValidJson_Happy_Path_Test()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void DeepClone_Happy_Path_Test()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void PrettyPrint_Happy_Path_Test()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void Minify_Happy_Path_Test()
+        {
+            // Test implementation here
+        }
+    }
+}
