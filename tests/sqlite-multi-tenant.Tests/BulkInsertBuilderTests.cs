4683771c01d1a3a8d612e492a13ccd6c7a28b816
Vladyslav Zaiets <rutova2@gmail.com>
2026-08-22 19:39:19 +0000

    chore: maintenance updates

--- tests/sqlite-multi-tenant.Tests/BulkInsertBuilderTests.cs
+++ tests/sqlite-multi-tenant.Tests/BulkInsertBuilderTests.cs
@@ -24,61 +24,83 @@ namespace SqliteMultiTenant.Tests.Operations
         [Fact]
         public void BulkInsertBuilder_Constructor_WithNullConnection_ThrowsArgumentNullException()
         {
+            _logger.LogInformation("Executing test: {TestName}", nameof(BulkInsertBuilder_Constructor_WithNullConnection_ThrowsArgumentNullException));
+
             // Act
             Action act = () => new BulkInsertBuilder(null!, _logger, TestTable);
 
             // Assert
             act.Should().Throw<ArgumentNullException>()
                 .WithParameterName("connection");
+
+            _logger.LogInformation("Test completed: {TestName}", nameof(BulkInsertBuilder_Constructor_WithNullConnection_ThrowsArgumentNullException));
         }
 
         [Fact]
         public void BulkInsertBuilder_Constructor_WithNullLogger_ThrowsArgumentNullException()
         {
+            _logger.LogInformation("Executing test: {TestName}", nameof(BulkInsertBuilder_Constructor_WithNullLogger_ThrowsArgumentNullException));
+
             // Act
             Action act = () => new BulkInsertBuilder(_connection, null!, TestTable);
 
             // Assert
             act.Should().Throw<ArgumentNullException>()
                 .WithParameterName("logger");
+
+            _logger.LogInformation("Test completed: {TestName}", nameof(BulkInsertBuilder_Constructor_WithNullLogger_ThrowsArgumentNullException));
         }
 
         [Fact]
         public void BulkInsertBuilder_Constructor_WithEmptyTableName_ThrowsArgumentException()
         {
+            _logger.LogInformation("Executing test: {TestName}", nameof(BulkInsertBuilder_Constructor_WithEmptyTableName_ThrowsArgumentException));
+
             // Act
             Action act = () => new BulkInsertBuilder(_connection, _logger, "");
 
             // Assert
             act.Should().Throw<ArgumentException>()
                 .WithMessage("Table name cannot be empty*");
+
+            _logger.LogInformation("Test completed: {TestName}", nameof(BulkInsertBuilder_Constructor_WithEmptyTableName_ThrowsArgumentException));
         }
 
         [Fact]
         public void BulkInsertBuilder_Constructor_WithNullTableName_ThrowsArgumentException()
         {
+            _logger.LogInformation("Executing test: {TestName}", nameof(BulkInsertBuilder_Constructor_WithNullTableName_ThrowsArgumentException));
+
             // Act
             Action act = () => new BulkInsertBuilder(_connection, _logger, null!);
 
             // Assert
             act.Should().Throw<ArgumentException>()
                 .WithMessage("Table name cannot be empty*");
+
+            _logger.LogInformation("Test completed: {TestName}", nameof(BulkInsertBuilder_Constructor_WithNullTableName_ThrowsArgumentException));
         }
 
         [Fact]
         public void BulkInsertBuilder_Constructor_WithWhitespaceTableName_ThrowsArgumentException()
         {
+            _logger.LogInformation("Executing test: {TestName}", nameof(BulkInsertBuilder_Constructor_WithWhitespaceTableName_ThrowsArgumentException));
+
             // Act
             Action act = () => new BulkInsertBuilder(_connection, _logger, "   ");
 
             // Assert
             act.Should().Throw<ArgumentException>()
                 .WithMessage("Table name cannot be empty*");
+
+            _logger.LogInformation("Test completed: {TestName}", nameof(BulkInsertBuilder_Constructor_WithWhitespaceTableName_ThrowsArgumentException));
         }
 
         [Fact]
         public void BulkInsertBuilder_AddRecord_WithNullRecord_ThrowsArgumentNullException()
         {
+            _logger.LogInformation("Executing test: {TestName}", nameof(BulkInsertBuilder_AddRecord_WithNullRecord_ThrowsArgumentNullException));
+
             // Arrange
             var builder = new BulkInsertBuilder(_connection, _logger, TestTable);
 
@@ -88,11 +110,15 @@ namespace SqliteMultiTenant.Tests.Operations
             // Assert
             act.Should().Throw<ArgumentNullException>()
                 .WithParameterName("record");
+
+            _logger.LogInformation("Test completed: {TestName}", nameof(BulkInsertBuilder_AddRecord_WithNullRecord_ThrowsArgumentNullException));
         }
 
         [Fact]
         public void BulkInsertBuilder_AddRecords_WithNullRecords_ThrowsArgumentNullException()
         {
+            _logger.LogInformation("Executing test: {TestName}", nameof(BulkInsertBuilder_AddRecords_WithNullRecords_ThrowsArgumentNullException));
+
             // Arrange
             var builder = new BulkInsertBuilder(_connection, _logger, TestTable);
 
@@ -102,11 +128,15 @@ namespace SqliteMultiTenant.Tests.Operations
             // Assert
             act.Should().Throw<ArgumentNullException>()
                 .WithParameterName("records");
+
+            _logger.LogInformation("Test completed: {TestName}", nameof(BulkInsertBuilder_AddRecords_WithNullRecords_ThrowsArgumentNullException));
         }
 
         [Fact]
         public void BulkInsertBuilder_GenerateSqlStatements_WithEmptyRecords_ReturnsEmptyString()
         {
+            _logger.LogInformation("Executing test: {TestName}", nameof(BulkInsertBuilder_GenerateSqlStatements_WithEmptyRecords_ReturnsEmptyString));
+
             // Arrange
             var builder = new BulkInsertBuilder(_connection, _logger, TestTable);
 
@@ -115,11 +145,15 @@ namespace SqliteMultiTenant.Tests.Operations
 
             // Assert
             sql.Should().BeEmpty();
+
+            _logger.LogInformation("Test completed: {TestName}", nameof(BulkInsertBuilder_GenerateSqlStatements_WithEmptyRecords_ReturnsEmptyString));
         }
 
         [Fact]
         public void BulkInsertBuilder_GenerateSqlStatements_WithSingleRecord_GeneratesCorrectSql()
         {
+            _logger.LogInformation("Executing test: {TestName}", nameof(BulkInsertBuilder_GenerateSqlStatements_WithSingleRecord_GeneratesCorrectSql));
+
             // Arrange
             var builder = new BulkInsertBuilder(_connection, _logger, TestTable);
             var record = new Dictionary<string, object>
@@ -138,11 +172,15 @@ namespace SqliteMultiTenant.Tests.Operations
             sql.Should().Contain("'1'");
             sql.Should().Contain("'Test'");
             sql.Should().Contain("'42.5'");
+
+            _logger.LogInformation("Test completed: {TestName}", nameof(BulkInsertBuilder_GenerateSqlStatements_WithSingleRecord_GeneratesCorrectSql));
         }
 
         [Fact]
         public void BulkInsertBuilder_GenerateSqlStatements_WithMultipleRecords_GeneratesCorrectSql()
         {
+            _logger.LogInformation("Executing test: {TestName}", nameof(BulkInsertBuilder_GenerateSqlStatements_WithMultipleRecords_GeneratesCorrectSql));
+
             // Arrange
             var builder = new BulkInsertBuilder(_connection, _logger, TestTable);
             builder.AddRecord(new Dictionary<string, object> { { "Id", 1 }, { "Name", "Test1" } });
@@ -156,11 +194,15 @@ namespace SqliteMultiTenant.Tests.Operations
             sql.Should().Contain("'1', 'Test1'");
             sql.Should().Contain("'2', 'Test2'");
             sql.Should().Contain("'3', 'Test3'");
+
+            _logger.LogInformation("Test completed: {TestName}", nameof(BulkInsertBuilder_GenerateSqlStatements_WithMultipleRecords_GeneratesCorrectSql));
         }
 
         [Fact]
         public void BulkInsertBuilder_GenerateSqlStatements_WithNullValues_HandlesCorrectly()
         {
+            _logger.LogInformation("Executing test: {TestName}", nameof(BulkInsertBuilder_GenerateSqlStatements_WithNullValues_HandlesCorrectly));
+
             // Arrange
             var builder = new BulkInsertBuilder(_connection, _logger, TestTable);
             var record = new Dictionary<string, object>
@@ -178,11 +220,15 @@ namespace SqliteMultiTenant.Tests.Operations
             sql.Should().StartWith($"INSERT INTO [{TestTable}] ([Id], [Name], [Value]) VALUES");
             sql.Should().Contain("'1'");
             sql.Should().Contain("NULL");
+
+            _logger.LogInformation("Test completed: {TestName}", nameof(BulkInsertBuilder_GenerateSqlStatements_WithNullValues_HandlesCorrectly));
         }
 
         [Fact]
         public void BulkInsertBuilder_GenerateSqlStatements_WithSpecialCharactersInValues_EscapesCorrectly()
         {
+            _logger.LogInformation("Executing test: {TestName}", nameof(BulkInsertBuilder_GenerateSqlStatements_WithSpecialCharactersInValues_EscapesCorrectly));
+
             // Arrange
             var builder = new BulkInsertBuilder(_connection, _logger, TestTable);
             var record = new Dictionary<string, object>
@@ -199,11 +245,15 @@ namespace SqliteMultiTenant.Tests.Operations
             // Assert
             sql.Should().Contain("'Test''O''''Leary'");
             sql.Should().Contain("It''s");
+
+            _logger.LogInformation("Test completed: {TestName}", nameof(BulkInsertBuilder_GenerateSqlStatements_WithSpecialCharactersInValues_EscapesCorrectly));
         }
 
         [Fact]
         public void BulkInsertBuilder_AddRecord_ReturnsBuilderForFluentInterface()
         {
+            _logger.LogInformation("Executing test: {TestName}", nameof(BulkInsertBuilder_AddRecord_ReturnsBuilderForFluentInterface));
+
             // Arrange
             var builder = new BulkInsertBuilder(_connection, _logger, TestTable);
             var record = new Dictionary<string, object> { { "Id", 1 } };
@@ -213,11 +263,15 @@ namespace SqliteMultiTenant.Tests.Operations
 
             // Assert
             result.Should().BeSameAs(builder);
+
+            _logger.LogInformation("Test completed: {TestName}", nameof(BulkInsertBuilder_AddRecord_ReturnsBuilderForFluentInterface));
         }
 
         [Fact]
         public void BulkInsertBuilder_AddRecords_ReturnsBuilderForFluentInterface()
         {
+            _logger.LogInformation("Executing test: {TestName}", nameof(BulkInsertBuilder_AddRecords_ReturnsBuilderForFluentInterface));
+
             // Arrange
             var builder = new BulkInsertBuilder(_connection, _logger, TestTable);
             var records = new List<Dictionary<string, object>>
@@ -231,6 +285,8 @@ namespace SqliteMultiTenant.Tests.Operations
 
             // Assert
             result.Should().BeSameAs(builder);
+
+            _logger.LogInformation("Test completed: {TestName}", nameof(BulkInsertBuilder_AddRecords_ReturnsBuilderForFluentInterface));
         }
     }
 }
