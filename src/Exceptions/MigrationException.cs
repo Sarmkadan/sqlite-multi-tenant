5684631251b4d7c8171e43579932ebf9f820241b
Vladyslav Zaiets <rutova2@gmail.com>
2026-09-09 07:50:15 +0000

    docs: add XML documentation to MigrationException public members

--- src/Exceptions/MigrationException.cs
+++ src/Exceptions/MigrationException.cs
@@ -11,15 +11,31 @@ namespace SqliteMultiTenant.Exceptions;
 /// </summary>
 public sealed class MigrationException : MultiTenantException
 {
+    /// <summary>
+    /// Gets the migration identifier.
+    /// </summary>
     public string? MigrationId { get; }
+
+    /// <summary>
+    /// Gets the migration version.
+    /// </summary>
     public string? MigrationVersion { get; }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MigrationException"/> class with a specified error message.
+    /// </summary>
+    /// <param name="message">The message that describes the error.</param>
     public MigrationException(string message)
         : base(message)
     {
         ArgumentException.ThrowIfNullOrEmpty(nameof(message));
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MigrationException"/> class with a specified error message and a reference to the inner exception that is the cause of this exception.
+    /// </summary>
+    /// <param name="message">The error message that explains the reason for the exception.</param>
+    /// <param name="innerException">The exception that is the cause of the current exception.</param>
     public MigrationException(string message, Exception innerException)
         : base(message, innerException)
     {
@@ -27,6 +43,13 @@ public sealed class MigrationException : MultiTenantException
         ArgumentNullException.ThrowIfNull(nameof(innerException));
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MigrationException"/> class with specified error message, migration ID, version, and optional inner exception.
+    /// </summary>
+    /// <param name="message">The error message.</param>
+    /// <param name="migrationId">The identifier of the migration.</param>
+    /// <param name="version">The version of the migration.</param>
+    /// <param name="innerException">The exception that is the cause of the current exception.</param>
     public MigrationException(string message, string migrationId, string? version, Exception? innerException = null)
         : base(message, innerException)
     {
