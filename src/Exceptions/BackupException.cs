5187b903c5a47e1a249d992095bf458f44aaf0d0
Vladyslav Zaiets <rutova2@gmail.com>
2026-09-09 06:54:10 +0000

    chore: apply codex edits

--- src/Exceptions/BackupException.cs
+++ src/Exceptions/BackupException.cs
@@ -11,21 +11,44 @@ namespace SqliteMultiTenant.Exceptions;
 /// </summary>
 public sealed class BackupException : MultiTenantException
 {
+    /// <summary>
+    /// Gets the identifier of the backup associated with the failure.
+    /// </summary>
     public string? BackupId { get; }
+
+    /// <summary>
+    /// Gets the identifier of the database associated with the backup operation.
+    /// </summary>
     public string? DatabaseId { get; }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="BackupException"/> class with a specified error message.
+    /// </summary>
+    /// <param name="message">The message that describes the error.</param>
     public BackupException(string message)
         : base(message)
     {
         ArgumentException.ThrowIfNullOrEmpty(message);
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="BackupException"/> class with a specified error message and inner exception.
+    /// </summary>
+    /// <param name="message">The message that describes the error.</param>
+    /// <param name="innerException">The exception that caused the current exception.</param>
     public BackupException(string message, Exception innerException)
         : base(message, innerException)
     {
         ArgumentException.ThrowIfNullOrEmpty(message);
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="BackupException"/> class with backup and database context.
+    /// </summary>
+    /// <param name="message">The message that describes the error.</param>
+    /// <param name="backupId">The identifier of the backup associated with the failure.</param>
+    /// <param name="databaseId">The identifier of the database associated with the backup operation.</param>
+    /// <param name="innerException">The exception that caused the current exception, or <see langword="null"/>.</param>
     public BackupException(string message, string backupId, string databaseId, Exception? innerException = null)
         : base(message, innerException)
     {
