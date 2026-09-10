e5c7b9bba37a4826b0561f80c2d5253a70a9f542
Vladyslav Zaiets <rutova2@gmail.com>
2026-09-10 06:01:35 +0000

    chore: maintenance updates

--- src/Security/EncryptionOptions.cs
+++ src/Security/EncryptionOptions.cs
@@ -1,10 +1,35 @@
 namespace SqliteMultiTenant.Security;
 
+/// <summary>
+/// Options for encryption configuration.
+/// </summary>
 public class EncryptionOptions
 {
+    /// <summary>
+    /// Gets or sets the size of the encryption key in bits.
+    /// </summary>
     public int KeySize { get; set; } = 256;
+
+    /// <summary>
+    /// Gets or sets the size of the initialization vector in bits.
+    /// </summary>
     public int IvSize { get; set; } = 128;
+
+    /// <summary>
+    /// Gets or sets the size of the salt in bits.
+    /// </summary>
     public int SaltSize { get; set; } = 128;
+
+    /// <summary>
+    /// Gets or sets the number of iterations for key derivation.
+    /// </summary>
     public int Iterations { get; set; } = 10000;
+
+    /// <summary>
+    /// Gets or sets the salt string used for key derivation.
+    /// </summary>
     public string DerivationSalt { get; set; } = "SqliteMultiTenant";
+
+    public override string ToString() =>
+        $"EncryptionOptions {{ KeySize = {KeySize}, IvSize = {IvSize}, SaltSize = {SaltSize}, Iterations = {Iterations}, DerivationSalt = {DerivationSalt} }}";
 }
